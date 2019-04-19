a2276141bae0309b2a30a1f4046802be629c49cf
董淑伟 <dsw0602@foxmail.com>
2019-04-19 18:47:11 +0800

    rearchitect the project to make it more moodular

---
+++ node/src/main.rs
@@ -0,0 +1,66 @@
+// Copyright 2018 Commonwealth Labs, Inc.
+// This file is part of Edgeware.
+
+// Edgeware is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Edgeware is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Edgeware.  If not, see <http://www.gnu.org/licenses/>
+
+//! Substrate Node Template CLI library.
+
+#![warn(missing_docs)]
+
+extern crate evo_cli as cli;
+extern crate ctrlc;
+extern crate futures;
+
+#[macro_use]
+extern crate error_chain;
+
+use cli::{VersionInfo};
+use futures::sync::oneshot;
+use futures::{future, Future};
+
+use std::cell::RefCell;
+
+// handles ctrl-c
+struct Exit;
+impl cli::IntoExit for Exit {
+	type Exit = future::MapErr<oneshot::Receiver<()>, fn(oneshot::Canceled) -> ()>;
+	fn into_exit(self) -> Self::Exit {
+		// can't use signal directly here because CtrlC takes only `Fn`.
+		let (exit_send, exit) = oneshot::channel();
+
+		let exit_send_cell = RefCell::new(Some(exit_send));
+		ctrlc::set_handler(move || {
+			if let Some(exit_send) = exit_send_cell.try_borrow_mut().expect("signal handler not reentrant; qed").take() {
+				exit_send.send(()).expect("Error sending exit notification");
+			}
+		}).expect("Error setting Ctrl-C handler");
+
+		exit.map_err(drop)
+	}
+}
+
+error_chain::quick_main!(run);
+
+fn run() -> cli::error::Result<()> {
+	let version = VersionInfo {
+		name: "EvolutionLand",
+		commit: env!("VERGEN_SHA_SHORT"),
+		version: env!("CARGO_PKG_VERSION"),
+		executable_name: "evoland",
+		author: "Commonwealth Labs <hello@commonwealth.im>",
+		description: "EvoLand Client Node",
+		support_url: "",
+	};
+	cli::run(::std::env::args(), Exit, version)
+}
