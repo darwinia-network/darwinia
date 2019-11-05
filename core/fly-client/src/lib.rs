b78fe277a8756833cd2cf39fa156a51844552282
Xavier Lau <c.estlavie@icloud.com>
2019-11-05 18:24:35 +0800

    update: new branch

--- core/fly-client/src/lib.rs
+++ core/fly-client/src/lib.rs
@@ -1,7 +1,1 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![cfg_attr(not(feature = "std"), no_std)]
--- srml/chainrelay/src/lib.rs
+++ srml/chainrelay/src/lib.rs
@@ -3,11 +3,15 @@
 #![recursion_limit = "128"]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use rstd::prelude::*;
+use rstd::vec::Vec;
 
+// use blake2::Blake2b;
+use codec::{Decode, Encode};
 use support::{decl_event, decl_module, decl_storage, dispatch::Result};
+use system::ensure_signed;
 
 use merkle_mountain_range::Hash;
+//use merkle_mountain_range::MerkleMountainRange;
 
 pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -15,7 +19,8 @@ pub trait Trait: system::Trait {
 
 decl_storage! {
 	trait Store for Module<T: Trait> as Bridge {
-
+		pub ChainHeaders get(chain_headers): map Chain => Vec<Header>;
+		pub RelayerContribution get(relayer_contribution): map T::AccountId => u32;
 	}
 }
 
@@ -24,7 +29,15 @@ decl_module! {
 	where
 		origin: T::Origin
 	{
+		pub fn submit_header(origin, chain: Chain, header: Header) {
+			let relayer = ensure_signed(origin)?;
+			let _ = Self::verify()?;
 
+			<RelayerContribution<T>>::mutate(relayer, |r_c| {
+				*r_c += 1;
+			});
+			ChainHeaders::mutate(chain, |c_h| c_h.push(header));
+		}
 	}
 }
 
@@ -37,4 +50,26 @@ decl_event! {
 	}
 }
 
-//pub struct
+impl<T: Trait> Module<T> {
+	fn verify() -> Result {
+		/// 1. if exists?
+		/// 2. verify (difficulty + prev_hash + nonce)
+		/// 3. challenge
+		unimplemented!()
+	}
+}
+
+// pub type MMR = MerkleMountainRange<Blake2b>;
+
+#[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
+pub enum Chain {
+	Ethereum,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
+pub struct Header {
+	difficulty: u32,
+	nonce: u32,
+	prev_hash: Hash,
+	merkle_root: Hash,
+}
