fe175a611c3d19493e1b366877eee7e9b609639b
Xavier Lau <c.estlavie@icloud.com>
2019-11-08 11:42:51 +0800

    try: trait design

--- srml/chainrelay/src/lib.rs
+++ srml/chainrelay/src/lib.rs
@@ -3,26 +3,42 @@
 #![recursion_limit = "128"]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-// for `vec![]` macro
-use rstd::vec;
-use rstd::vec::Vec;
+pub mod bridge;
 
 // use blake2::Blake2b;
 use codec::{Decode, Encode};
-use support::{decl_event, decl_module, decl_storage, dispatch::Result};
+use support::{
+	decl_event, decl_module, decl_storage,
+	dispatch::Result,
+	traits::{Currency, LockableCurrency},
+};
 use system::ensure_signed;
 
-use merkle_mountain_range::Hash;
-//use merkle_mountain_range::MerkleMountainRange;
+//use bridge::{Bridge, Relayer};
+use bridge::{EOSBridge as EOSBridgeT, EOSRelayer as EOSRelayerT};
+use bridge::{EthereumBridge as EthereumBridgeT, EthereumRelayer as EthereumRelayerT};
+//use merkle_mountain_range::{MerkleMountainRange, Hash};
+use crate::bridge::Bridge;
+use TargetChain::*;
 
 pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+	type Ring: LockableCurrency<Self::AccountId, Moment = Self::BlockNumber>;
 }
 
+// config() require `serde = { version = "1.0.101", optional = true }`
+// tracking issue: https://github.com/rust-lang/rust/issues/27812
 decl_storage! {
 	trait Store for Module<T: Trait> as Bridge {
-		pub ChainHeaders get(chain_headers): map Chain => Vec<Header> = vec![];
-		pub RelayerContribution get(relayer_contribution): map T::AccountId => u32 = 0;
+		pub DepositPool get(deposit_pool): RingBalanceOf<T>;
+
+		pub EOSBridge get(eos_bridge): EOSBridgeT;
+		pub EOSDepositValue get(eos_deposit_value) config(): RingBalanceOf<T>;
+		pub EOSRelayer get(eos_relayer): EOSRelayerT;
+
+		pub EthereumBridge get(ethereum_bridge): EthereumBridgeT;
+		pub EthereumDepositValue get(ethereum_deposit_value) config(): RingBalanceOf<T>;
+		pub EthereumRelayer get(etnereum_relayer): EthereumRelayerT;
 	}
 }
 
@@ -31,14 +47,26 @@ decl_module! {
 	where
 		origin: T::Origin
 	{
-		pub fn submit_header(origin, chain: Chain, header: Header) {
-			let relayer = ensure_signed(origin)?;
+		pub fn submit_header(origin, target_chain: TargetChain, header: Header) {
+			let _relayer = ensure_signed(origin)?;
 			let _ = Self::verify(&header)?;
 
-			<RelayerContribution<T>>::mutate(relayer, |r_c| {
-				*r_c += 1;
-			});
-			ChainHeaders::mutate(chain, |c_h| c_h.push(header));
+			{
+				match target_chain {
+					EOS => {}
+					Ethereum => {}
+				}
+			}
+		}
+
+		// `Darwinia lock` corresponds to `TargetChain redeem`
+		pub fn lock(origin, _target_chain: TargetChain) {
+			let _locker = ensure_signed(origin)?;
+		}
+
+		// `Darwinia redeem` corresponds to `TargetChain lock`
+		pub fn redeem(origin, _target_chain: TargetChain, _header: Header) {
+			let _redeemer = ensure_signed(origin)?;
 		}
 	}
 }
@@ -53,26 +81,46 @@ decl_event! {
 }
 
 impl<T: Trait> Module<T> {
+	fn _adjust_deposit_value(_: TargetChain, _value: RingBalanceOf<T>) -> Result {
+		unimplemented!()
+	}
+
 	/// 1. if exists?
 	/// 2. verify (difficulty + prev_hash + nonce)
 	/// 3. challenge
-	fn verify(header: &Header) -> Result {
-		Ok(())
+	fn verify(_: &Header) -> Result {
+		unimplemented!()
+	}
+
+	fn _punish(_who: &T::AccountId) -> Result {
+		unimplemented!()
+	}
+
+	fn _release(_dest: &T::AccountId, _value: RingBalanceOf<T>) -> Result {
+		unimplemented!()
+	}
+
+	fn _match_chain_with<F: FnOnce(&mut dyn Bridge)>(target_chain: TargetChain, f: F) {
+		match target_chain {
+			EOS => {
+				EOSBridge::mutate(|eos_bridge| f(eos_bridge));
+			}
+			Ethereum => {}
+		}
+
+		unimplemented!()
 	}
 }
+
+type RingBalanceOf<T> = <<T as Trait>::Ring as Currency<<T as system::Trait>::AccountId>>::Balance;
+// TODO: type
+type Header = ();
 
 // FIXME: currently, use SPV instead
 // pub type MMR = MerkleMountainRange<Blake2b>;
 
 #[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
-pub enum Chain {
+pub enum TargetChain {
+	EOS,
 	Ethereum,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq, Encode, Decode)]
-pub struct Header {
-	difficulty: u32,
-	nonce: u32,
-	prev_hash: Hash,
-	merkle_root: Hash,
 }
