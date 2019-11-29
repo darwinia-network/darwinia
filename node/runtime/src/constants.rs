bbf455b7835842ad459cf7f57ee2a1aecb46441e
Xavier Lau <c.estlavie@icloud.com>
2019-11-29 12:48:06 +0800

    update: block time

--- node/runtime/src/constants.rs
+++ node/runtime/src/constants.rs
@@ -49,9 +49,9 @@ pub mod time {
 	/// <https://research.web3.foundation/en/latest/polkadot/BABE/Babe/#6-practical-results>
 
 	// Test
-	pub const MILLISECS_PER_BLOCK: Moment = 1000;
+	//	pub const MILLISECS_PER_BLOCK: Moment = 1000;
 	// Production
-	//	pub const MILLISECS_PER_BLOCK: Moment = 3000;
+	pub const MILLISECS_PER_BLOCK: Moment = 3000;
 	pub const SECS_PER_BLOCK: Moment = MILLISECS_PER_BLOCK / 1000;
 
 	pub const SLOT_DURATION: Moment = MILLISECS_PER_BLOCK;
