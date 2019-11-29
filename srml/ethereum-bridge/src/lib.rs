2b45066114e8544f3b74379173b5c73b5fbc6898
HackFisher <hackfisher@gmail.com>
2019-11-29 15:21:54 +0800

    minor changes, commit before merge

--- srml/ethereum-bridge/src/lib.rs
+++ srml/ethereum-bridge/src/lib.rs
@@ -7,14 +7,20 @@
 use codec::{Decode, Encode};
 use rstd::vec::Vec;
 use sr_eth_primitives::{
-	header::EthHeader, BestBlock, BlockNumber as EthBlockNumber, H160, H256, H64, U128, U256, U512,
+	header::EthHeader, pow::EthashPartial, pow::EthashSeal, BestBlock, BlockNumber as EthBlockNumber, H160, H256, H64,
+	U128, U256, U512,
 };
+
+use ethash::{EthereumPatch, LightDAG};
+
 use support::{decl_event, decl_module, decl_storage, dispatch::Result, traits::Currency};
 use system::ensure_signed;
 
 use sr_primitives::RuntimeDebug;
 
 use rlp::{decode, encode};
+
+type DAG = LightDAG<EthereumPatch>;
 
 pub trait Trait: system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -36,11 +42,11 @@ pub struct ActionRecord {
 decl_storage! {
 	trait Store for Module<T: Trait> as EthBridge {
 		/// Anchor block that works as genesis block
-		pub BeginHeader get(begin_header): Option<EthHeader>;
+		pub BeginHeader get(fn begin_header): Option<EthHeader>;
 		/// Info of the best block header for now
-		pub BestHeader get(best_header): BestBlock;
+		pub BestHeader get(fn best_header): BestBlock;
 		///
-		pub BlockList get(block_list): map EthBlockNumber => EthHeader;
+		pub BlockList get(fn block_list): map EthBlockNumber => EthHeader;
 
 		pub HeaderOf get(header_of): map H256 => Option<EthHeader>;
 
@@ -62,8 +68,6 @@ decl_storage! {
 				BeginHeader::put(header.clone());
 
 				<Module<T>>::genesis_header(header);
-			} else {
-//				BeginNumber::put(config.number);
 			}
 		});
 	}
@@ -76,14 +80,14 @@ decl_module! {
 	{
 		fn deposit_event() = default;
 
-		pub fn store_block_header(origin, header: EthHeader) {
+		pub fn relay_header(origin, header: EthHeader) {
 			let _relayer = ensure_signed(origin)?;
 			let _ = Self::verify(&header)?;
 
 			<Module<T>>::deposit_event(RawEvent::NewHeader(header));
 		}
 
-		pub fn relay_receipt(origin, proof: ActionRecord) {
+		pub fn check_receipt(origin, proof: ActionRecord) {
 			// confirm that the block hash is right
 			// get the receipt MPT trie root from the block header
 			// Using receipt MPT trie root to verify the proof and index etc.
@@ -123,7 +127,50 @@ impl<T: Trait> Module<T> {
 	/// 1. if exists?
 	/// 2. verify (difficulty + prev_hash + nonce + re-org)
 	/// 3. challenge
-	fn verify(header: &EthHeader) -> Result {
+	fn verify_header(header: &EthHeader) -> Result {
+		// check parent hash,
+		let parent_hash = header.parent_hash();
+
+		match <HeaderOf<T>>::get(parent_hash) {
+			None => return Err("Parent Header Not Found."),
+			Some(parent_header) => {
+				if parent_header.hash() != parent_hash {
+					return Err("Parent hash does not match.");
+				}
+			}
+		}
+
+		// check difficulty
+		let mut ethash_params = EthashPartial::expanse();
+		ethash_params.set_difficulty_bomb_delays(0xc3500, 5000000);
+		let result = ethash_params.verify_block_basic(header);
+		match result {
+			Ok() => {}
+			Err(e) => {
+				return Err(e);
+			}
+		}
+
+		// verify difficulty
+		//		let difficulty = ethash_params.calculate_difficulty(header, &prev_header);
+		//		ensure!(difficulty == header.difficulty(), "difficulty verification failed");
+
+		// verify mixhash
+		let number = header.number();
+		let seal = EthashSeal::parse_seal(header.seal())?;
+
+		let light_dag = DAG::new(number.into());
+		let partial_header_hash = header.bare_hash();
+		let mix_hash = light_dag.hashimoto(partial_header_hash, seal.nonce);
+
+		if mix_hash != seal.mix_hash {
+			return Err("Mixhash does not match.");
+		}
+
+		let header_hash = header.hash();
+
+		<HeaderOf<I>>::insert(header_hash, header);
+
 		let number = header.number();
 		Ok(())
 	}
