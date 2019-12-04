3bf795deff49fd5c14efd1c3caf6fc120d1b7232
Xavier Lau <c.estlavie@icloud.com>
2019-12-04 10:53:04 +0800

    add: native types

--- types/src/main.rs
+++ types/src/main.rs
@@ -5,6 +5,18 @@ use std::{
 	path::Path,
 };
 
+mod native_types {
+	const ARRAY: &[&str] = &["Bytes", "H64", "H160", "H256", "U128", "U256", "U512"];
+
+	const NUMERIC: &[&str] = &[
+		"i8", "u8", "i16", "u16", "i32", "u32", "i64", "u64", "i128", "u128", "i268", "u256",
+	];
+
+	const SUBSTRATE: &[&str] = &["Address", "LockIdentifier"];
+
+	const WRAPPER: &[&str] = &[r"Compact<.+?>", r"Option<.+?>", r"Vec<.+?>", r"[.+?;.+?]"];
+}
+
 fn main() -> io::Result<()> {
 	let srml_path = env::args()
 		.skip(1)
@@ -35,25 +47,6 @@ fn main() -> io::Result<()> {
 		.collect::<Vec<_>>();
 
 	println!("{:?}", modules);
-
-	//	let srml_dir = fs::read_dir(
-	//		env::args()
-	//			.skip(1)
-	//			.next()
-	//			.ok_or(io::Error::new(
-	//				io::ErrorKind::NotFound,
-	//				"please specify the `srml` path",
-	//			))?
-	//			.as_str(),
-	//	)?
-	//	.into_iter()
-	//	.filter_map(|entry| if let Ok(entry) = entry { Some(entry) } else { None })
-	//	.find(|entry| {
-	//		let path = entry.path();
-	//		println!("{:?}", path);
-	//		path.is_dir() && path.file_name().unwrap().to_str().unwrap() == "srml"
-	//	})
-	//	.ok_or(io::Error::new(io::ErrorKind::NotFound, "folder `srml` not found"))?;
 
 	Ok(())
 }
@@ -101,11 +94,11 @@ struct Storage {
 impl Storage {
 	fn from_file<S: AsRef<Path>>(path: S) -> io::Result<Self> {
 		let mut file = File::open(path)?;
-		let mut s = String::new();
-		file.read_to_string(&mut s)?;
+		let mut text = String::new();
+		file.read_to_string(&mut text)?;
 
 		Ok(Self {
-			structs: Storage::parse_structs(s),
+			structs: Storage::parse_structs(text),
 		})
 	}
 
