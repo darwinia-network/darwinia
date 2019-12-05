12682e47782aec57da19ec98f2953c1609d7f7dd
readlnh <readlnh@readlnhdeMBP.lan>
2019-12-05 16:36:14 +0800

    fix balances tests

--- srml/balances/src/lib.rs
+++ srml/balances/src/lib.rs
@@ -37,7 +37,9 @@ use support::{
 };
 use system::{ensure_root, ensure_signed, IsDeadAccount, OnNewAccount};
 
+#[cfg(all(feature = "std", test))]
 mod mock;
+#[cfg(all(feature = "std", test))]
 mod tests;
 
 pub use self::imbalances::{NegativeImbalance, PositiveImbalance};
