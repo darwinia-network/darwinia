c2dd3577e875952eb52662ed13fbc3530e2a01ee
Xavier Lau <c.estlavie@icloud.com>
2019-12-10 18:14:46 +0800

    add: `reward_from_authorship_event_handler_works`

--- srml/staking/src/tests.rs
+++ srml/staking/src/tests.rs
@@ -1987,6 +1987,31 @@ fn new_era_elects_correct_number_of_validators() {
 		})
 }
 
+#[test]
+fn reward_from_authorship_event_handler_works() {
+	ExtBuilder::default().build().execute_with(|| {
+		use authorship::EventHandler;
+
+		assert_eq!(<authorship::Module<Test>>::author(), 11);
+
+		<Module<Test>>::note_author(11);
+		<Module<Test>>::note_uncle(21, 1);
+		// An uncle author that is not currently elected doesn't get rewards,
+		// but the block producer does get reward for referencing it.
+		<Module<Test>>::note_uncle(31, 1);
+		// Rewarding the same two times works.
+		<Module<Test>>::note_uncle(11, 1);
+
+		// Not mandatory but must be coherent with rewards
+		assert_eq!(<CurrentElected<Test>>::get(), vec![21, 11]);
+
+		// 21 is rewarded as an uncle producer
+		// 11 is rewarded as a block producer and uncle referencer and uncle producer
+		assert_eq!(CurrentEraPointsEarned::get().individual, vec![1, 20 + 2 * 3 + 1]);
+		assert_eq!(CurrentEraPointsEarned::get().total, 28);
+	})
+}
+
 //#[test]
 //fn normal_kton_should_work() {
 //	ExtBuilder::default().existential_deposit(0).build().execute_with(|| {
