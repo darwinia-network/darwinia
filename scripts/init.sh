6402dc14a21b5ad7c89139b1ed86aa6d34bca99f
Xavier Lau <c.estlavie@icloud.com>
2019-12-12 17:18:10 +0800

    update: script

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
+    rustup default nightly
+    rustup update nightly
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly
+rustup target add wasm32-unknown-unknown
