13f2753cb4835efdf69d77acfb40127800298e0d
HackFisher <hackfisher@gmail.com>
2019-12-13 19:16:49 +0800

    parsing the log

--- srml/eth-backing/src/lib.rs
+++ srml/eth-backing/src/lib.rs
@@ -9,11 +9,14 @@ use rstd::prelude::*;
 use support::{decl_event, decl_module, decl_storage, dispatch::Result, traits::Currency};
 use system::ensure_signed;
 
+use sr_primitives::AccountId32;
 use sr_primitives::RuntimeDebug;
+
+use core::convert::TryFrom;
 
 use darwinia_eth_relay::{ActionRecord, VerifyEthReceipts};
 use darwinia_support::{LockableCurrency, OnDepositRedeem};
-use ethabi::{Event as EthEvent, EventParam as EthEventParam, ParamType};
+use ethabi::{Event as EthEvent, EventParam as EthEventParam, ParamType, RawLog};
 use sr_eth_primitives::{receipt::LogEntry, receipt::Receipt, Address, H256, U256};
 
 //#[cfg(feature = "std")]
@@ -22,6 +25,14 @@ use sr_eth_primitives::{receipt::LogEntry, receipt::Receipt, Address, H256, U256
 use hex_literal::hex;
 
 pub type Moment = u64;
+
+type RingBalanceOf<T> = <<T as Trait>::Ring as Currency<<T as system::Trait>::AccountId>>::Balance;
+type RingPositiveImbalanceOf<T> = <<T as Trait>::Ring as Currency<<T as system::Trait>::AccountId>>::PositiveImbalance;
+type RingNegativeImbalanceOf<T> = <<T as Trait>::Ring as Currency<<T as system::Trait>::AccountId>>::NegativeImbalance;
+
+type KtonBalanceOf<T> = <<T as Trait>::Kton as Currency<<T as system::Trait>::AccountId>>::Balance;
+type KtonPositiveImbalanceOf<T> = <<T as Trait>::Kton as Currency<<T as system::Trait>::AccountId>>::PositiveImbalance;
+type KtonNegativeImbalanceOf<T> = <<T as Trait>::Kton as Currency<<T as system::Trait>::AccountId>>::NegativeImbalance;
 
 pub trait Trait: timestamp::Trait {
 	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
@@ -47,7 +58,8 @@ decl_storage! {
 		pub KtonRedeemTopic get(kton_redeem_topic): Option<EventTopic>;
 		pub DepositRedeemTopic get(deposit_redeem_topic): Option<EventTopic>;
 
-		// TODO: cache redeem history to avoid re-request attack.
+		pub RingLocked get(fn ring_locked): RingBalanceOf<T>;
+		pub KtonLocked get(fn kton_locked): KtonBalanceOf<T>;
 	}
 	add_extra_genesis {
 		config(genesis_ring_redeem_topic): Option<Vec<u8>>;
@@ -81,68 +93,6 @@ decl_storage! {
 	}
 }
 
-decl_module! {
-	pub struct Module<T: Trait> for enum Call
-	where
-		origin: T::Origin
-	{
-		pub fn redeem_ring(origin, proof_record: ActionRecord) {
-			let _relayer = ensure_signed(origin)?;
-
-			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
-
-			let event = EthEvent {
-				name: "foo".to_owned(),
-				inputs: vec![EthEventParam {
-					name: "a".to_owned(),
-					kind: ParamType::Int(256),
-					indexed: false,
-				}, EthEventParam {
-					name: "b".to_owned(),
-					kind: ParamType::Int(256),
-					indexed: true,
-				}, EthEventParam {
-					name: "c".to_owned(),
-					kind: ParamType::Address,
-					indexed: false,
-				}, EthEventParam {
-					name: "d".to_owned(),
-					kind: ParamType::Address,
-					indexed: true,
-				}, EthEventParam {
-					name: "e".to_owned(),
-					kind: ParamType::String,
-					indexed: true,
-				}, EthEventParam {
-					name: "f".to_owned(),
-					kind: ParamType::Array(Box::new(ParamType::Int(256))),
-					indexed: true
-				}, EthEventParam {
-					name: "g".to_owned(),
-					kind: ParamType::FixedArray(Box::new(ParamType::Address), 5),
-					indexed: true,
-				}],
-				anonymous: false,
-			};
-
-			// if header confirmed then return
-			// if header in unverified header then challenge
-		}
-
-		pub fn redeem_kton(origin, proof_record: ActionRecord) {
-			let _locker = ensure_signed(origin)?;
-
-			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
-		}
-
-		pub fn redeem_deposit(origin, proof_record: ActionRecord) {
-			let _redeemer = ensure_signed(origin)?;
-
-			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
-		}
-	}
-}
-
 decl_event! {
 	pub enum Event<T>
 	where
@@ -159,5 +109,83 @@ impl<T: Trait> Module<T> {
 
 	//	fn _release(_dest: &T::AccountId, _value: RingBalanceOf<T>) -> Result {
 	//		unimplemented!()
+	//	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call
+	where
+		origin: T::Origin
+	{
+		pub fn redeem_ring(origin, proof_record: ActionRecord) {
+			let _relayer = ensure_signed(origin)?;
+
+			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
+
+			let log_entry = verified_receipt.logs.iter().find(
+				|&x| x.address == Address::from(hex!("dbc888d701167cbfb86486c516aafbefc3a4de6e"))
+					 && x.topics[0] == H256::from(hex!("38045eaef0a21b74ff176350f18df02d9041a25d6694b5f63e9474b7b6cd6b94"))
+			).expect("Log Entry Not Found");
+
+			// event RingBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
+			let eth_event = EthEvent {
+				name: "RingBurndropTokens".to_owned(),
+				inputs: vec![EthEventParam {
+					name: "token".to_owned(),
+					kind: ParamType::Address,
+					indexed: true,
+				}, EthEventParam {
+					name: "owner".to_owned(),
+					kind: ParamType::Address,
+					indexed: true,
+				}, EthEventParam {
+					name: "amount".to_owned(),
+					kind: ParamType::Uint(256),
+					indexed: false,
+				}, EthEventParam {
+					name: "data".to_owned(),
+					kind: ParamType::Bytes,
+					indexed: false,
+				}],
+				anonymous: false,
+			};
+
+			let log = RawLog {
+				topics: [log_entry.topics[0],log_entry.topics[1],log_entry.topics[2]].to_vec(),
+				data: log_entry.data.clone()
+			};
+
+			let result = eth_event.parse_log(log).expect("Parse Eth Log Error");
+
+			let amount : U256 = result.params[2].value.clone().to_uint().expect("Param Convert to Int Failed.");
+			let raw_sub_key : Vec<u8> = result.params[3].value.clone().to_bytes().expect("Param Convert to Bytes Failed.");
+
+			let darwinia_account: AccountId32 = AccountId32::try_from(raw_sub_key.as_slice()).expect("Account Parse Failed.");
+//			let mut r = T::AccountId::default();
+//			r.0.copy_from_slice(raw_sub_key.as_slice());
+
+//			let darwinia_account: AccountId32 = hex!["d7b504ddbe25a05647312daa8d0bbbafba360686241b7e193ca90f9b01f95faa"].into();
+
+// Self::Ring::issue(raw_sub_key, amount);
+		}
+
+		pub fn redeem_kton(origin, proof_record: ActionRecord) {
+			let _locker = ensure_signed(origin)?;
+
+			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
+		}
+
+		pub fn redeem_deposit(origin, proof_record: ActionRecord) {
+			let _redeemer = ensure_signed(origin)?;
+
+			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
+		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+	//	pub fn to_hex_string(bytes: Vec<u8>) -> String {
+	//		let strs: Vec<String> = bytes.iter().map(|b| format!("{:02x}", b)).collect();
+	//		strs.join("")
 	//	}
 }
