7110aa47c594d08b27d411718b80afda77d7dd29
Xavier Lau <c.estlavie@icloud.com>
2019-12-17 12:45:35 +0800

    update: refactoring `redeem_ring`, `redeem_kton`

--- srml/eth-backing/src/lib.rs
+++ srml/eth-backing/src/lib.rs
@@ -5,12 +5,11 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 //use codec::{Decode, Encode};
-use rstd::{borrow::ToOwned, result};
-use rstd::{marker::PhantomData, prelude::*}; // fmt::Debug,
+use ethabi::{Event as EthEvent, EventParam as EthEventParam, ParamType, RawLog};
+use rstd::{borrow::ToOwned, convert::TryFrom, marker::PhantomData, result, vec::Vec}; // fmt::Debug
+use sr_primitives::traits::{SaturatedConversion, Saturating};
 use support::{decl_event, decl_module, decl_storage, ensure, traits::Currency, traits::OnUnbalanced}; // dispatch::Result,
-use system::ensure_signed;
-
-use sr_primitives::traits::{SaturatedConversion, Saturating}; // Convert,
+use system::ensure_signed; // Convert,
 
 //use sr_primitives::RuntimeDebug;
 //use primitives::crypto::UncheckedFrom;
@@ -18,14 +17,12 @@ use sr_primitives::traits::{SaturatedConversion, Saturating}; // Convert,
 
 use darwinia_eth_relay::{EthReceiptProof, VerifyEthReceipts};
 use darwinia_support::{LockableCurrency, OnDepositRedeem};
-use ethabi::{Event as EthEvent, EventParam as EthEventParam, ParamType, RawLog};
 use sr_eth_primitives::{EthAddress, H256, U256}; // receipt::LogEntry, receipt::Receipt,
 
 //#[cfg(feature = "std")]
 //use sr_primitives::{Deserialize, Serialize};
 
-use rstd::vec::Vec;
-
+pub type Balance = u128;
 pub type Moment = u64;
 
 type RingBalanceOf<T> = <<T as Trait>::Ring as Currency<<T as system::Trait>::AccountId>>::Balance;
@@ -89,109 +86,42 @@ decl_module! {
 		pub fn redeem_ring(origin, proof_record: EthReceiptProof) {
 			let _relayer = ensure_signed(origin)?;
 
-			ensure!(!<RingProofVerified>::exists((proof_record.header_hash, proof_record.index)), "Ring for this proof has already been redeemed.");
+			ensure!(
+				!RingProofVerified::exists((proof_record.header_hash, proof_record.index)),
+				"Ring For This Proof - ALREADY BEEN REDEEMED",
+			);
 
-			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
+			let (darwinia_account, redeemed_amount) = Self::parse_proof(&proof_record, "RingBurndropTokens")?;
+			let redeemed_ring = <RingBalanceOf<T>>::saturated_from(redeemed_amount);
+			let redeemed_positive_imbalance_ring = T::Ring::deposit_into_existing(&darwinia_account, redeemed_ring)?;
 
-			// event RingBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
-			// https://ropsten.etherscan.io/tx/0x81f699c93b00ab0b7db701f87b6f6045c1e0692862fcaaf8f06755abb0536800
-			let eth_event = EthEvent {
-				name: "RingBurndropTokens".to_owned(),
-				inputs: vec![
-					EthEventParam {name: "token".to_owned(), kind: ParamType::Address, indexed: true,},
-					EthEventParam {name: "owner".to_owned(), kind: ParamType::Address, indexed: true,},
-					EthEventParam {name: "amount".to_owned(), kind: ParamType::Uint(256), indexed: false,},
-					EthEventParam {name: "data".to_owned(), kind: ParamType::Bytes, indexed: false,}
-				],
-				anonymous: false,
-			};
+			T::RingReward::on_unbalanced(redeemed_positive_imbalance_ring);
 
-			// H256::from(hex!("38045eaef0a21b74ff176350f18df02d9041a25d6694b5f63e9474b7b6cd6b94")
-			let log_entry = verified_receipt.logs.iter().find(
-				|&x| x.address == Self::ring_redeem_address()
-					 && x.topics[0] == eth_event.signature()
-			).expect("Log Entry Not Found");
-
-			let log = RawLog {
-				topics: [log_entry.topics[0],log_entry.topics[1],log_entry.topics[2]].to_vec(),
-				data: log_entry.data.clone()
-			};
-
-			let result = eth_event.parse_log(log).expect("Parse Eth Log Error");
-
-			// TODO: div 10**18 and mul 10**9
-			let mut amount : U256 = result.params[2].value.clone().to_uint().expect("Param Convert to Int Failed.");
-			amount = amount / U256::from(1_000_000_000u64);
-
-			let raw_sub_key : Vec<u8> = result.params[3].value.clone().to_bytes().expect("Param Convert to Bytes Failed.");
-
-			let decoded_sub_key = hex::decode(&raw_sub_key[..]).expect("Address Hex decode Failed.");
-			let darwinia_account = T::DetermineAccountId::account_id_for(&decoded_sub_key[..])?;
-
-			let redeemed_amount = amount.as_u128().saturated_into();
-
+			RingProofVerified::insert((proof_record.header_hash, proof_record.index), proof_record);
 			<RingLocked<T>>::mutate(|l| {
-				*l = l.saturating_sub(redeemed_amount);
+				*l = l.saturating_sub(redeemed_ring);
 			});
-
-			<RingProofVerified>::insert((proof_record.header_hash, proof_record.index), proof_record);
-
-			let redeemed_ring = T::Ring::deposit_into_existing(&darwinia_account, redeemed_amount).expect("Deposit into existing failed.");
-
-			T::RingReward::on_unbalanced(redeemed_ring);
 		}
 
 		// event KtonBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
 		pub fn redeem_kton(origin, proof_record: EthReceiptProof) {
 			let _relayer = ensure_signed(origin)?;
 
-			ensure!(!<KtonProofVerified>::exists((proof_record.header_hash, proof_record.index)), "Kton for this proof has already been redeemed.");
+			ensure!(
+				!KtonProofVerified::exists((proof_record.header_hash, proof_record.index)),
+				"Kton for this proof has already been redeemed.",
+			);
 
-			let verified_receipt = T::EthRelay::verify_receipt(&proof_record)?;
+			let (darwinia_account, redeemed_amount) = Self::parse_proof(&proof_record, "KtonBurndropTokens")?;
+			let redeemed_kton = <KtonBalanceOf<T>>::saturated_from(redeemed_amount);
+			let redeemed_positive_imbalance_kton = T::Kton::deposit_into_existing(&darwinia_account, redeemed_kton)?;
 
-			// event RingBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
-			// https://ropsten.etherscan.io/tx/0x81f699c93b00ab0b7db701f87b6f6045c1e0692862fcaaf8f06755abb0536800
-			let eth_event = EthEvent {
-				name: "KtonBurndropTokens".to_owned(),
-				inputs: vec![
-					EthEventParam {name: "token".to_owned(), kind: ParamType::Address, indexed: true,},
-					EthEventParam {name: "owner".to_owned(), kind: ParamType::Address, indexed: true,},
-					EthEventParam {name: "amount".to_owned(), kind: ParamType::Uint(256), indexed: false,},
-					EthEventParam {name: "data".to_owned(), kind: ParamType::Bytes, indexed: false,}
-				],
-				anonymous: false,
-			};
+			T::KtonReward::on_unbalanced(redeemed_positive_imbalance_kton);
 
-			let log_entry = verified_receipt.logs.iter().find(
-				|&x| x.address == Self::kton_redeem_address()
-					 && x.topics[0] == eth_event.signature()
-			).expect("Log Entry Not Found");
-
-			let log = RawLog {
-				topics: [log_entry.topics[0],log_entry.topics[1],log_entry.topics[2]].to_vec(),
-				data: log_entry.data.clone()
-			};
-
-			let result = eth_event.parse_log(log).expect("Parse Eth Log Error");
-			// TODO: div 10**18 and mul 10**9
-			let mut amount : U256 = result.params[2].value.clone().to_uint().expect("Param Convert to Int Failed.");
-			amount = amount / U256::from(1_000_000_000u64);
-			let raw_sub_key : Vec<u8> = result.params[3].value.clone().to_bytes().expect("Param Convert to Bytes Failed.");
-
-			let decoded_sub_key = hex::decode(&raw_sub_key[..]).expect("Address Hex decode Failed.");
-			let darwinia_account = T::DetermineAccountId::account_id_for(&decoded_sub_key[..])?;
-
-			let redeemed_amount = amount.as_u128().saturated_into();
-
+			KtonProofVerified::insert((proof_record.header_hash, proof_record.index), proof_record);
 			<KtonLocked<T>>::mutate(|l| {
-				*l = l.saturating_sub(redeemed_amount);
+				*l = l.saturating_sub(redeemed_kton);
 			});
-
-			<KtonProofVerified>::insert((proof_record.header_hash, proof_record.index), proof_record);
-
-			let redeemed_kton = T::Kton::deposit_into_existing(&darwinia_account, redeemed_amount).expect("Deposit into existing failed.");
-
-			T::KtonReward::on_unbalanced(redeemed_kton);
 		}
 
 		// https://github.com/evolutionlandorg/bank
@@ -263,6 +193,79 @@ decl_module! {
 
 			T::OnDepositRedeem::on_deposit_redeem(month.saturated_into(), start_at.saturated_into(), amount.as_u128(), &darwinia_account)?;
 		}
+	}
+}
+
+impl<T: Trait> Module<T> {
+	fn parse_proof(proof: &EthReceiptProof, event_name: &str) -> result::Result<(T::AccountId, Balance), &'static str> {
+		let verified_receipt = T::EthRelay::verify_receipt(proof)?;
+
+		// event RingBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
+		// https://ropsten.etherscan.io/tx/0x81f699c93b00ab0b7db701f87b6f6045c1e0692862fcaaf8f06755abb0536800
+		// event KtonBurndropTokens(address indexed token, address indexed owner, uint amount, bytes data);
+		// TODO: address
+		let result = {
+			let eth_event = EthEvent {
+				name: event_name.to_owned(),
+				inputs: vec![
+					EthEventParam {
+						name: "token".to_owned(),
+						kind: ParamType::Address,
+						indexed: true,
+					},
+					EthEventParam {
+						name: "owner".to_owned(),
+						kind: ParamType::Address,
+						indexed: true,
+					},
+					EthEventParam {
+						name: "amount".to_owned(),
+						kind: ParamType::Uint(256),
+						indexed: false,
+					},
+					EthEventParam {
+						name: "data".to_owned(),
+						kind: ParamType::Bytes,
+						indexed: false,
+					},
+				],
+				anonymous: false,
+			};
+			let log_entry = verified_receipt
+				.logs
+				.into_iter()
+				.find(|x| x.address == Self::ring_redeem_address() && x.topics[0] == eth_event.signature())
+				.ok_or("Log Entry - NOT FOUND")?;
+			let log = RawLog {
+				topics: vec![log_entry.topics[0], log_entry.topics[1], log_entry.topics[2]],
+				data: log_entry.data.clone(),
+			};
+
+			eth_event.parse_log(log).map_err(|_| "Parse Eth Log - FAILED")?
+		};
+		let redeemed_amount = {
+			// TODO: div 10**18 and mul 10**9
+			let amount = result.params[2]
+				.value
+				.clone()
+				.to_uint()
+				.map(|x| x / U256::from(1_000_000_000u64))
+				.ok_or("Convert to Int - FAILED")?;
+
+			Balance::try_from(amount)?
+		};
+		let darwinia_account = {
+			let raw_sub_key = result.params[3]
+				.value
+				.clone()
+				.to_bytes()
+				.ok_or("Convert to Bytes - FAILED")?;
+			let decoded_sub_key = hex::decode(&raw_sub_key).map_err(|_| "Decode Address - FAILED")?;
+
+			T::DetermineAccountId::account_id_for(&decoded_sub_key)?
+		};
+
+		Ok((darwinia_account, redeemed_amount))
 	}
 }
 
