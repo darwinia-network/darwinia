b5437f999c9a306e17524741f63c86cc6b688559
Xavier Lau <c.estlavie@icloud.com>
2019-12-26 18:52:32 +0800

    try: fix ci

--- ci/script.sh
+++ ci/script.sh
@@ -8,21 +8,27 @@ curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain=$1 -y
 # Load cargo environment. Specifically, put cargo into PATH.
 source ~/.cargo/env
 
-rustc --version
+# Make sure using the nightly toolchain
+rustup default nightly
+
+# Install wasm toolchain
+rustup target add wasm32-unknown-unknown
+
 rustup --version
 cargo --version
+rustc --version
 
 case $2 in
-	"native")
-		sudo apt-get -y update
-		sudo apt-get install -y cmake pkg-config libssl-dev
+  "native")
+    sudo apt-get -y update
+    sudo apt-get install -y cmake pkg-config libssl-dev
 
-		cargo test --all --locked
-		;;
+    # Unit test
+    cargo test --release --all --locked
+    ;;
 
-	"wasm")
-		# Install prerequisites and build all wasm projects
-		./init.sh
-		./build.sh --locked
-		;;
+  "wasm")
+    # Build test
+    cargo build
+    ;;
 esac
