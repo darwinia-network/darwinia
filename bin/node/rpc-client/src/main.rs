ec9eeb8e6d3bd3af187be8d449b2fe7926e11372
Xavier Lau <c.estlavie@icloud.com>
2020-03-06 17:44:10 +0800

    update: rpc-client to substrate-alpha.3

--- bin/node/rpc-client/src/main.rs
+++ bin/node/rpc-client/src/main.rs
@@ -1,25 +1,9 @@
-// Copyright 2019-2020 Parity Technologies (UK) Ltd.
-// This file is part of Substrate.
-
-// Substrate is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Substrate is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
-
 #![warn(missing_docs)]
 
-//! Example darwinia RPC client code.
+//! Example substrate RPC client code.
 //!
 //! This module shows how you can write a Rust RPC client that connects to a running
-//! darwinia node and use staticly typed RPC wrappers.
+//! substrate node and use statically typed RPC wrappers.
 
 use futures::Future;
 use hyper::rt;
@@ -47,7 +31,7 @@ fn main() {
 /// 1. Calls the `pending_extrinsics` method to get all extrinsics in the pool.
 /// 2. Then calls `remove_extrinsic` passing the obtained raw extrinsics.
 ///
-/// As the resul of running the code the entire content of the transaction pool is going
+/// As the result of running the code the entire content of the transaction pool is going
 /// to be removed and the extrinsics are going to be temporarily banned.
 fn remove_all_extrinsics(client: AuthorClient<Hash, Hash>) -> impl Future<Item = (), Error = RpcError> {
 	client
