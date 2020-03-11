961500708ba27bb36c5941d16c163d96f16469f5
Xavier Lau <c.estlavie@icloud.com>
2020-03-12 00:04:59 +0800

    add: claims list formatter

---
+++ .maintain/utility/format_claims_list.py
@@ -0,0 +1,26 @@
+import json
+
+def append_format(origin: str, content: dict) -> str:
+	for k, v in content.items():
+		origin += '{{ "k": "{}", "v": {} }},'.format(k, v)
+
+	return origin
+
+with open('genesis_sample.json') as f:
+	j = json.load(f)
+
+n_j = '{ "eth": ['
+n_j = append_format(n_j, j['eth'])
+n_j = n_j[:-1] + '],'
+
+n_j += '"tron": ['
+n_j = append_format(n_j, j['tron'])
+n_j = n_j[:-1] + '],'
+
+n_j += '"dot": ['
+n_j = append_format(n_j, j['dot'])
+n_j = n_j[:-1] + ']}'
+
+with open('claims_list.json', 'w+') as f:
+	f.write(n_j)
+
