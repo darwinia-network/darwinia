cf4c7be1cf6671144cc4f4ecd7140e9023a5dbaf
Xavier Lau <c.estlavie@icloud.com>
2020-03-25 16:59:06 +0800

    remove: WASM targets (#394)

--- .maintain/clean-offchain-builds.py
+++ .maintain/clean-offchain-builds.py
@@ -3,11 +3,12 @@ from os.path import abspath, dirname
 from pathlib import Path
 from shutil import rmtree
 
-for pattern in ['*eth_offchain*']:
-    for path in iglob(''.join([dirname(dirname(abspath(__file__))), '/target', '/release', '/**/', pattern]), recursive=True):
-        print('removed:', path)
-        path = Path(path)
-        if path.is_dir():
-            rmtree(path)
-        elif path.is_file:
-            path.unlink()
+for mode in ['debug', 'release']:
+    for pattern in ['*eth_offchain*']:
+        for path in iglob(''.join([dirname(dirname(abspath(__file__))), '/target/', mode, '/**/', pattern]), recursive=True):
+            print('removed:', path)
+            path = Path(path)
+            if path.is_dir():
+                rmtree(path)
+            elif path.is_file:
+                path.unlink()
