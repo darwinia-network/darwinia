a01dbe353c4448590f8df705c551965ac6b266a6
clearloop <udtrokia@gmail.com>
2020-03-26 09:17:38 +0800

    update: simplify methods in eth-relay tests

--- frame/chainrelay/eth/relay/src/mock_headers/utils.rs
+++ frame/chainrelay/eth/relay/src/mock_headers/utils.rs
@@ -1,121 +1,81 @@
 //! utils for parsing eth headers
-use std::{fmt::Debug, str::FromStr};
-
-use eth_primitives::{Bloom, EthAddress, H64};
-use hex::FromHex as HexFromHex;
-use json::object::Object;
-use rustc_hex::FromHex;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
 use crate::*;
 
-pub use eth_primitives::receipt::LogEntry;
-pub use json::JsonValue;
-
-/// get hash
-fn get_hash(o: &mut Object, key: &str) -> Option<H256> {
-	Some(H256::from(<[u8; 32]>::from_hex(&o.remove(key)?.as_str()?[2..]).ro()?))
-}
-
-/// convert Result to Option
-///
-/// NOTE: panic should happen in tests, but not mock.
-trait ResultToOption<T> {
-	fn ro(self) -> Option<T>;
-}
-
-impl<T, E> ResultToOption<T> for Result<T, E>
-where
-	E: Debug,
-{
-	fn ro(self) -> Option<T> {
-		match self {
-			Ok(v) => Some(v),
-			Err(_) => None,
-		}
-	}
-}
+pub use eth_primitives::{receipt::LogEntry, Bloom, EthAddress, H64};
+pub use json::{object::Object, JsonValue};
 
 /// mock ethheader
-pub fn mock_header_from_source<'m>(o: &'m mut Object) -> Option<EthHeader> {
-	let mixh = get_hash(o, "mixHash")?;
-	let nonce = H64::from(<[u8; 8]>::from_hex(&o.remove("nonce")?.as_str()?[2..]).ro()?);
-	let addr = <[u8; 20]>::from_hex(&o.remove("miner")?.as_str()?[2..]).ro()?;
-
-	let extra_data = o.remove("extraData")?;
-	let eds = &extra_data.as_str()?[2..];
-
-	let log_bloom = o.remove("logsBloom")?;
-	let lb = &log_bloom.as_str()?[2..];
-
-	let difficulty: u64 = o.remove("difficulty")?.as_str()?.parse().ro()?;
+pub fn mock_header_from_source<'m>(o: &'m Object) -> Option<EthHeader> {
+	let mixh = H256::from(bytes!(&o.get("mixHash")?, 32));
+	let nonce = H64::from(bytes!(&o.get("nonce")?, 8));
 
 	Some(EthHeader {
-		parent_hash: get_hash(o, "parentHash")?,
-		timestamp: o.remove("timestamp")?.as_u64()?,
-		number: o.remove("number")?.as_u64()?,
-		author: EthAddress::from(addr),
-		transactions_root: get_hash(o, "transactionsRoot")?,
-		uncles_hash: get_hash(o, "sha3Uncles")?,
-		extra_data: eds.from_hex().unwrap(),
-		state_root: get_hash(o, "stateRoot")?,
-		receipts_root: get_hash(o, "receiptsRoot")?,
-		log_bloom: Bloom::from_str(lb).unwrap(),
-		gas_used: o.remove("gasUsed")?.as_i32()?.into(),
-		gas_limit: o.remove("gasLimit")?.as_i32()?.into(),
-		difficulty: difficulty.into(),
+		parent_hash: H256::from(bytes!(&o.get("parentHash")?, 32)),
+		timestamp: o.get("timestamp")?.as_u64()?,
+		number: o.get("number")?.as_u64()?,
+		author: EthAddress::from(bytes!(&o.get("miner")?, 20)),
+		transactions_root: H256::from(bytes!(&o.get("transactionsRoot")?, 32)),
+		uncles_hash: H256::from(bytes!(&o.get("sha3Uncles")?, 32)),
+		extra_data: hex(&o.get("extraData")?.as_str()?)?,
+		state_root: H256::from(bytes!(&o.get("stateRoot")?, 32)),
+		receipts_root: H256::from(bytes!(&o.get("receiptsRoot")?, 32)),
+		log_bloom: Bloom::from_str(&o.get("logsBloom")?.as_str()?[2..]).unwrap(),
+		gas_used: o.get("gasUsed")?.as_i32()?.into(),
+		gas_limit: o.get("gasLimit")?.as_i32()?.into(),
+		difficulty: o.get("difficulty")?.as_str()?.parse::<u64>().unwrap_or(0).into(),
 		seal: vec![rlp::encode(&mixh), rlp::encode(&nonce)],
-		hash: Some(get_hash(o, "hash")?),
+		hash: Some(H256::from(bytes!(&o.get("hash")?, 32))),
 	})
 }
 
 /// mock receipt
 pub fn mock_receipt_from_source(o: &mut Object) -> Option<EthReceiptProof> {
-	let index: u64 = {
-		let i = o.remove("index")?;
-		if let Ok(idx) = i.as_str()?[2..].parse() {
-			idx
-		} else {
-			return None;
-		}
-	};
-
-	let proof: Vec<u8> = {
-		let p = o.remove("proof")?;
-		if let Ok(prf) = p.as_str()?[2..].from_hex() {
-			prf
-		} else {
-			return None;
-		}
-	};
-
 	Some(EthReceiptProof {
-		index,
-		proof,
-		header_hash: get_hash(o, "header_hash")?,
+		index: o.get("index")?.as_str()?[2..].parse::<u64>().unwrap(),
+		proof: hex(&o.get("proof")?.as_str()?)?,
+		header_hash: H256::from(bytes!(&o.get("header_hash")?, 32)),
 	})
 }
 
 /// mock logs
 pub fn mock_log_from_source(o: &mut Object) -> Option<LogEntry> {
-	let data: Vec<u8> = {
-		let p = o.remove("data")?;
-		if let Ok(d) = p.as_str()?[2..].from_hex() {
-			d
-		} else {
-			return None;
-		}
-	};
-
 	let mut topics: Vec<H256> = vec![];
 	if let JsonValue::Array(ts) = o.remove("topics")? {
 		for t in ts.iter() {
-			topics.push(H256::from(<[u8; 32]>::from_hex(&t.as_str()?[2..]).ro()?));
+			topics.push(H256::from(bytes!(t, 32)));
 		}
 	}
 
 	Some(LogEntry {
-		address: EthAddress::from(<[u8; 20]>::from_hex(&o.remove("address")?.as_str()?[2..]).ro()?),
+		address: EthAddress::from(bytes!(&o.get("address")?, 20)),
 		topics,
-		data,
+		data: hex(&o.get("data")?.as_str()?)?,
 	})
+}
+
+/// convert hex string to byte array
+fn hex(s: &str) -> Option<Vec<u8>> {
+	let r: Result<Vec<u8>, ParseIntError> = (2..s.len())
+		.step_by(2)
+		.map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+		.collect();
+
+	if let Ok(r) = r {
+		Some(r)
+	} else {
+		None
+	}
+}
+
+/// readable macro
+#[macro_export]
+macro_rules! bytes {
+	($str:expr, $len: tt) => {{
+		let mut bytes: [u8; $len] = [0; $len];
+		bytes.copy_from_slice(&hex(&$str.as_str()?)?);
+		bytes
+		}};
 }
