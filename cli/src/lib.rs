344f8222af92f0b2b52db1595e97395f7b57cecd
Xavier Lau <c.estlavie@icloud.com>
2020-04-13 23:47:11 +0800

    rollback: cli

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3,15 +3,16 @@
 #![warn(missing_docs)]
 #![warn(unused_extern_crates)]
 
+// pub mod chain_spec;
+
 #[cfg(feature = "browser")]
 mod browser;
+mod chain_spec;
 #[cfg(feature = "cli")]
 mod cli;
 #[cfg(feature = "cli")]
 mod command;
 
-#[cfg(feature = "cli")]
-pub use cli::*;
 #[cfg(feature = "cli")]
 pub use command::run;
 #[cfg(feature = "cli")]
