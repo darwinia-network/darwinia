0e36bb775b94c587cbb07eff9e7aa7446642a242
Xavier Lau <c.estlavie@icloud.com>
2020-06-03 09:57:03 +0800

    update: spec version

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -69,7 +69,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("Crab"),
 	impl_name: create_runtime_str!("Crab"),
 	authoring_version: 0,
-	spec_version: 2,
+	spec_version: 3,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
