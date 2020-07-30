175905351887705cc3912fc1a9273f5a0102ae77
Xavier Lau <c.estlavie@icloud.com>
2020-07-30 13:54:55 +0800

    update: common runtime

--- runtime/common/src/impls.rs
+++ runtime/common/src/impls.rs
@@ -3,11 +3,8 @@
 // --- crates ---
 use codec::{Decode, Encode};
 // --- substrate ---
-use frame_support::traits::{Currency, Get, Imbalance, OnUnbalanced};
-use sp_runtime::{
-	traits::{Convert, Saturating},
-	Fixed128, FixedPointNumber, Perquintill, RuntimeDebug,
-};
+use frame_support::traits::{Currency, Imbalance, OnUnbalanced};
+use sp_runtime::RuntimeDebug;
 // --- darwinia ---
 use crate::*;
 
@@ -25,7 +22,6 @@ darwinia_support::impl_account_data! {
 
 /// Logic for the author to get a portion of fees.
 pub struct ToAuthor<R>(sp_std::marker::PhantomData<R>);
-
 impl<R> OnUnbalanced<NegativeImbalance<R>> for ToAuthor<R>
 where
 	R: darwinia_balances::Trait<RingInstance> + pallet_authorship::Trait,
@@ -59,7 +55,6 @@ pub mod support_kton_in_the_future {
 
 	/// Converter for currencies to votes.
 	pub struct CurrencyToVoteHandler<R>(sp_std::marker::PhantomData<R>);
-
 	impl<R> CurrencyToVoteHandler<R>
 	where
 		R: darwinia_balances::Trait<RingInstance>,
@@ -71,7 +66,6 @@ pub mod support_kton_in_the_future {
 			(issuance / u64::max_value() as u128).max(1)
 		}
 	}
-
 	impl<R> Convert<u128, u64> for CurrencyToVoteHandler<R>
 	where
 		R: darwinia_balances::Trait<RingInstance>,
@@ -81,7 +75,6 @@ pub mod support_kton_in_the_future {
 			(x / Self::factor()) as u64
 		}
 	}
-
 	impl<R> Convert<u128, u128> for CurrencyToVoteHandler<R>
 	where
 		R: darwinia_balances::Trait<RingInstance>,
@@ -89,63 +82,6 @@ pub mod support_kton_in_the_future {
 	{
 		fn convert(x: u128) -> u128 {
 			x * Self::factor()
-		}
-	}
-}
-
-/// Update the given multiplier based on the following formula
-///
-///   diff = (previous_block_weight - target_weight)/max_weight
-///   v = 0.00004
-///   next_weight = weight * (1 + (v * diff) + (v * diff)^2 / 2)
-///
-/// Where `target_weight` must be given as the `Get` implementation of the `T` generic type.
-/// https://research.web3.foundation/en/latest/polkadot/Token%20Economics/#relay-chain-transaction-fees
-pub struct TargetedFeeAdjustment<T, R>(sp_std::marker::PhantomData<(T, R)>);
-
-impl<T: Get<Perquintill>, R: frame_system::Trait> Convert<Fixed128, Fixed128>
-	for TargetedFeeAdjustment<T, R>
-{
-	fn convert(multiplier: Fixed128) -> Fixed128 {
-		let max_weight = MaximumBlockWeight::get();
-		let block_weight = <frame_system::Module<R>>::block_weight()
-			.total()
-			.min(max_weight);
-		let target_weight = (T::get() * max_weight) as u128;
-		let block_weight = block_weight as u128;
-
-		// determines if the first_term is positive
-		let positive = block_weight >= target_weight;
-		let diff_abs = block_weight.max(target_weight) - block_weight.min(target_weight);
-		// safe, diff_abs cannot exceed u64 and it can always be computed safely even with the lossy
-		// `Fixed128::saturating_from_rational`.
-		let diff = Fixed128::saturating_from_rational(diff_abs, max_weight.max(1));
-		let diff_squared = diff.saturating_mul(diff);
-
-		// 0.00004 = 4/100_000 = 40_000/10^9
-		let v = Fixed128::saturating_from_rational(4, 100_000);
-		// 0.00004^2 = 16/10^10 Taking the future /2 into account... 8/10^10
-		let v_squared_2 = Fixed128::saturating_from_rational(8, 10_000_000_000u64);
-
-		let first_term = v.saturating_mul(diff);
-		let second_term = v_squared_2.saturating_mul(diff_squared);
-
-		if positive {
-			// Note: this is merely bounded by how big the multiplier and the inner value can go,
-			// not by any economical reasoning.
-			let excess = first_term.saturating_add(second_term);
-			multiplier.saturating_add(excess)
-		} else {
-			// Defensive-only: first_term > second_term. Safe subtraction.
-			let negative = first_term.saturating_sub(second_term);
-			multiplier
-				.saturating_sub(negative)
-				// despite the fact that apply_to saturates weight (final fee cannot go below 0)
-				// it is crucially important to stop here and don't further reduce the weight fee
-				// multiplier. While at -1, it means that the network is so un-congested that all
-				// transactions have no weight fee. We stop here and only increase if the network
-				// became more busy.
-				.max(Fixed128::saturating_from_integer(-1))
 		}
 	}
 }
--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -16,13 +16,16 @@ pub use sp_runtime::BuildStorage;
 #[cfg(feature = "std")]
 pub use darwinia_staking::StakerStatus;
 
+// --- crates ---
+use static_assertions::const_assert;
 // --- substrate ---
 use frame_support::{
 	parameter_types,
 	traits::Currency,
 	weights::{constants::WEIGHT_PER_SECOND, Weight},
 };
-use sp_runtime::{traits::Saturating, Perbill};
+use pallet_transaction_payment::{Multiplier, TargetedFeeAdjustment};
+use sp_runtime::{traits::Saturating, FixedPointNumber, Perbill, Perquintill};
 // --- darwinia ---
 use darwinia_primitives::BlockNumber;
 
@@ -33,11 +36,35 @@ pub type NegativeImbalance<T> = <darwinia_balances::Module<T, RingInstance> as C
 	<T as frame_system::Trait>::AccountId,
 >>::NegativeImbalance;
 
+/// We assume that an on-initialize consumes 10% of the weight on average, hence a single extrinsic
+/// will not be allowed to consume more than `AvailableBlockRatio - 10%`.
+const AVERAGE_ON_INITIALIZE_WEIGHT: Perbill = Perbill::from_percent(10);
 parameter_types! {
 	pub const BlockHashCount: BlockNumber = 2400;
+	/// Block time that can be used by weights.
 	pub const MaximumBlockWeight: Weight = 2 * WEIGHT_PER_SECOND;
-	pub const MaximumExtrinsicWeight: Weight = AvailableBlockRatio::get()
-		.saturating_sub(Perbill::from_percent(10)) * MaximumBlockWeight::get();
+	/// Maximum weight that a _single_ extrinsic can take.
+	pub MaximumExtrinsicWeight: Weight = AvailableBlockRatio::get()
+		.saturating_sub(AVERAGE_ON_INITIALIZE_WEIGHT) * MaximumBlockWeight::get();
 	pub const MaximumBlockLength: u32 = 5 * 1024 * 1024;
+	/// Portion of the block available to normal class of dispatches.
 	pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+	/// The portion of the `AvailableBlockRatio` that we adjust the fees with. Blocks filled less
+	/// than this will decrease the weight and more will increase.
+	pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+	/// The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
+	/// change the fees more rapidly.
+	pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(3, 100_000);
+	/// Minimum amount of the multiplier. This value cannot be too low. A test case should ensure
+	/// that combined with `AdjustmentVariable`, we can recover from the minimum.
+	/// See `multiplier_can_grow_from_zero`.
+	pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 1_000_000_000u128);
 }
+const_assert!(
+	AvailableBlockRatio::get().deconstruct() >= AVERAGE_ON_INITIALIZE_WEIGHT.deconstruct()
+);
+
+/// Parameterized slow adjusting fee updated based on
+/// https://w3f-research.readthedocs.io/en/latest/polkadot/Token%20Economics.html#-2.-slow-adjusting-mechanism
+pub type SlowAdjustingFeeUpdate<R> =
+	TargetedFeeAdjustment<R, TargetBlockFullness, AdjustmentVariable, MinimumMultiplier>;
