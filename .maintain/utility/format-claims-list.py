57f72db48339578c76ef493dfcc114b31f55adfe
Xavier Lau <c.estlavie@icloud.com>
2020-09-19 17:44:40 +0800

    update: format

--- .maintain/utility/format-claims-list.py
+++ .maintain/utility/format-claims-list.py
@@ -14,17 +14,17 @@ root_dir = dirname(dirname(dirname(abspath(__file__))))
 with open(''.join([root_dir, '/.maintain', '/utility', '/crab-claims-list.json'])) as f:
     j = load(f)
 
-n_j = '{ "dot": ['
+n_j = '{"dot":['
 n_j = append_format(n_j, j['dot'])
 n_j = n_j[:-1] + '],'
 
-n_j += '"eth": ['
+n_j += '"eth":['
 n_j = append_format(n_j, j['eth'])
 n_j = n_j[:-1] + '],'
 
-n_j += '"tron": ['
+n_j += '"tron":['
 n_j = append_format(n_j, j['tron'])
 n_j = n_j[:-1] + ']}'
 
-with open(''.join([root_dir, '/service', '/res', '/crab-claims-list.json']), 'w') as f:
+with open(''.join([root_dir, '/node', '/service', '/res', '/crab-claims-list.json']), 'w') as f:
     f.write(n_j)
