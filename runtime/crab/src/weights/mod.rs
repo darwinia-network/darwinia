e699cd1dc5fe53b46cc02f7d74297a615cfcca85
Xavier Lau <c.estlavie@icloud.com>
2020-09-22 18:11:39 +0800

    use: darwinia democracy

--- runtime/crab/src/weights/mod.rs
+++ runtime/crab/src/weights/mod.rs
@@ -1,8 +1,8 @@
 //! A collection of weight modules used for pallets in the runtime.
 
 pub mod darwinia_balances;
+pub mod darwinia_democracy;
 pub mod frame_system;
-pub mod pallet_democracy;
 pub mod pallet_proxy;
 pub mod pallet_timestamp;
 pub mod pallet_utility;
