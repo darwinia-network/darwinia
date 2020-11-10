5166ac6b49b0ef710fcdabc51fb9692ab89c33b7
Xavier Lau <c.estlavie@icloud.com>
2020-11-10 16:59:05 +0800

    Release 0.8.2 (#586)
    * bump: dependencies
    
    * bump: version
    
    * bump: chain spec

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("Crab"),
 	impl_name: create_runtime_str!("Darwinia Crab"),
 	authoring_version: 0,
-	spec_version: 27,
+	spec_version: 28,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
