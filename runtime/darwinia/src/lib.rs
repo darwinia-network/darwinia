d2a7465f29a081c988ce00ee4e97621028d81387
Xavier Lau <c.estlavie@icloud.com>
2020-12-31 15:19:37 +0800

    Release v0.8.4 (#593)
    * fix(crab runtime): contract address
    
    * chore(crab runtime): bump version
    
    * chore(darwinia runtime): bump spec version

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -172,7 +172,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("Darwinia"),
 	impl_name: create_runtime_str!("Darwinia"),
 	authoring_version: 0,
-	spec_version: 10,
+	spec_version: 11,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
