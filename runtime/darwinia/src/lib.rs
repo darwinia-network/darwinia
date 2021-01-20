3dc51b5004be647401e2c53e65a5eb4e8485ad70
Xavier Lau <c.estlavie@icloud.com>
2021-01-20 17:51:57 +0800

    soften the terms

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -953,14 +953,14 @@ impl darwinia_ethereum_backing::Trait for Runtime {
 type EthereumRelayAuthoritiesInstance = darwinia_relay_authorities::Instance0;
 parameter_types! {
 	pub const EthereumRelayAuthoritiesLockId: LockIdentifier = *b"ethrauth";
-	pub const EthereumRelayAuthoritiesTermDuration: BlockNumber = 30 * DAYS;
+	pub const EthereumRelayAuthoritiesTermDuration: BlockNumber = 7 * DAYS;
 	pub const MaxCandidates: usize = 7;
 	pub const OpCodes: (OpCode, OpCode) = (
 		[71, 159, 189, 249],
 		[180, 188, 244, 151]
 	);
 	pub const SignThreshold: Perbill = Perbill::from_percent(60);
-	pub const SubmitDuration: BlockNumber = 100;
+	pub const SubmitDuration: BlockNumber = 300;
 }
 impl darwinia_relay_authorities::Trait<EthereumRelayAuthoritiesInstance> for Runtime {
 	type Event = Event;
