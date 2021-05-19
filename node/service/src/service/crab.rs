9c9561afc68efd700cdd855646496b9480ab6b62
Xavier Lau <c.estlavie@icloud.com>
2021-05-19 19:49:23 +0800

    companion for #substrate-8643

--- node/service/src/service/crab.rs
+++ node/service/src/service/crab.rs
@@ -29,6 +29,7 @@ use std::{
 // --- crates ---
 use futures::stream::StreamExt;
 // --- substrate ---
+use sc_authority_discovery::WorkerConfig;
 use sc_basic_authorship::ProposerFactory;
 use sc_client_api::{BlockchainEvents, ExecutorProvider, RemoteBackend, StateBackendFor};
 use sc_consensus::LongestChain;
@@ -342,6 +343,7 @@ where
 
 	let prometheus_registry = config.prometheus_registry().cloned();
 	let shared_voter_state = rpc_setup;
+	let auth_disc_publish_non_global_ips = config.network.allow_non_globals_in_dht;
 
 	config
 		.network
@@ -488,13 +490,18 @@ where
 						_ => None,
 					}
 				});
-		let (authority_discovery_worker, _service) = sc_authority_discovery::new_worker_and_service(
-			client.clone(),
-			network,
-			Box::pin(dht_event_stream),
-			authority_discovery_role,
-			prometheus_registry,
-		);
+		let (authority_discovery_worker, _service) =
+			sc_authority_discovery::new_worker_and_service_with_config(
+				WorkerConfig {
+					publish_non_global_ips: auth_disc_publish_non_global_ips,
+					..Default::default()
+				},
+				client.clone(),
+				network,
+				Box::pin(dht_event_stream),
+				authority_discovery_role,
+				prometheus_registry,
+			);
 
 		task_manager.spawn_handle().spawn(
 			"authority-discovery-worker",
