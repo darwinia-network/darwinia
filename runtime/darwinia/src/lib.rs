7626f80cee27cbc5ffda0480e9a195acb4706a97
Xavier Lau <c.estlavie@icloud.com>
2021-05-19 08:47:21 +0800

    new `spec_version`

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -187,7 +187,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Darwinia"),
 	impl_name: sp_runtime::create_runtime_str!("Darwinia"),
 	authoring_version: 0,
-	spec_version: 24,
+	// crate version ~0.11.0 := >=0.11.0, <0.12.0
+	spec_version: 1100,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
