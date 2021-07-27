f0c47926d512bf0784411eb26c38b044b06310f8
Xavier Lau <hi@xavier.zone>
2021-07-28 07:59:51 +0800

    companion for #polkadot-2966

--- runtime/crab/src/pallets/offences.rs
+++ runtime/crab/src/pallets/offences.rs
@@ -1,19 +1,11 @@
 // --- substrate ---
-use frame_support::weights::Weight;
 use pallet_offences::Config;
 use pallet_session::historical::IdentificationTuple;
-use sp_runtime::Perbill;
 // --- darwinia ---
 use crate::*;
-
-frame_support::parameter_types! {
-	pub OffencesWeightSoftLimit: Weight = Perbill::from_percent(60)
-		* RuntimeBlockWeights::get().max_block;
-}
 
 impl Config for Runtime {
 	type Event = Event;
 	type IdentificationTuple = IdentificationTuple<Self>;
 	type OnOffenceHandler = Staking;
-	type WeightSoftLimit = OffencesWeightSoftLimit;
 }
