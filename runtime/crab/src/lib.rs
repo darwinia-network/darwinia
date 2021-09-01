103c281ba2237ca0f333c016a2bd2e981ba0853d
Xavier Lau <hi@xavier.zone>
2021-09-01 17:57:41 +0800

    Bump `transaction_version`

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -162,7 +162,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 7,
+	transaction_version: 8,
 };
 
 /// Native version.
--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -208,7 +208,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	apis: RUNTIME_API_VERSIONS,
 	#[cfg(feature = "disable-runtime-api")]
 	apis: sp_version::create_apis_vec![[]],
-	transaction_version: 3,
+	transaction_version: 4,
 };
 
 /// Native version.
