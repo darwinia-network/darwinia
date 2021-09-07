b6ab082233da396ac834c26d354fab2b1639f68f
Xavier Lau <hi@xavier.zone>
2021-09-07 14:30:05 +0800

    Fix Typo

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -599,7 +599,7 @@ fn migrate() -> Weight {
 	// const CrabBackingPalletId: PalletId = PalletId(*b"da/crabk");
 
 	migration::move_storage_from_pallet(b"Reasons", b"DarwiniaTreasury", b"Treasury");
-	log::info("TreasuryReasons Migrated");
+	log::info!("TreasuryReasons Migrated");
 
 	// 0
 	RuntimeBlockWeights::get().max_block
