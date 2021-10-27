b86303fe66e0a6f2f1ba8967d2ae264500459c31
fewensa <fewensa@protonmail.com>
2021-10-27 16:45:46 +0800

    Try ci

--- .maintain/setup-wasm.sh
+++ .maintain/setup-wasm.sh
@@ -11,15 +11,13 @@ WORK_PATH=${BIN_PATH}/../
 
 BRANCH_NAME=$(echo $GITHUB_REF | cut -d'/' -f 3)
 
-## 1. chevdor/srtool:nightly-2021-02-25 have a bug, --json and --app upside down, use other version need remove it.
-EXTRA_ARGS='--json'
-
 docker run --rm -i \
   -e PACKAGE=darwinia-runtime \
   -e VERBOSE=1 \
   -e CARGO_TERM_COLOR=always \
+  -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:${RUST_TOOLCHAIN} build ${EXTRA_ARGS} \
+  chevdor/srtool:1.53.0 build --app \
   | tee ${WORK_PATH}/build-darwinia-wasm.log
 
 
@@ -27,15 +25,16 @@ docker run --rm -i \
   -e PACKAGE=crab-runtime \
   -e VERBOSE=1 \
   -e CARGO_TERM_COLOR=always \
+  -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:${RUST_TOOLCHAIN} build ${EXTRA_ARGS} \
+  chevdor/srtool:1.53.0 build --app \
   | tee ${WORK_PATH}/build-crab-wasm.log
 
 
 mkdir -p ${WORK_PATH}/deploy/bin
 
-_PROPOSAL_DARWINIA=$(cat ${WORK_PATH}/build-darwinia-wasm.log | grep 'Proposal\s\+:')
-_PROPOSAL_CRAB=$(cat ${WORK_PATH}/build-crab-wasm.log | grep 'Proposal\s\+:')
+_PROPOSAL_DARWINIA=$(cat ${WORK_PATH}/build-darwinia-wasm.log | grep 'prop\s\+:')
+_PROPOSAL_CRAB=$(cat ${WORK_PATH}/build-crab-wasm.log | grep 'prop\s\+:')
 
 PROPOSAL_DARWINIA=0x${_PROPOSAL_DARWINIA#*0x}
 PROPOSAL_CRAB=0x${_PROPOSAL_CRAB#*0x}
@@ -46,6 +45,10 @@ PROPOSAL_CRAB=$(echo ${PROPOSAL_CRAB} | sed 's/[^[:print:]]\[0m//g')
 
 echo ${PROPOSAL_DARWINIA} > ${WORK_PATH}/deploy/bin/${PROPOSAL_DARWINIA}.proposal.darwinia.txt
 echo ${PROPOSAL_CRAB} > ${WORK_PATH}/deploy/bin/${PROPOSAL_CRAB}.proposal.crab.txt
+
+tree ${WORK_PATH}/target/srtool/release/
+echo  ----
+tree ${WORK_PATH}/runtime/
 
 ### before srtool 2021-03-15 use this
 cp ${WORK_PATH}/target/srtool/release/wbuild/darwinia-runtime/darwinia_runtime.compact.wasm \
