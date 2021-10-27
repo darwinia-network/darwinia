3d7f1ade0d8e97abb4d51c8177d659003de10d17
ouwenkg <2630582710@qq.com>
2021-10-27 21:10:56 +0800

    Rename DynamicFeeParameters

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -69,7 +69,7 @@ pub struct RunCmd {
 
 	#[allow(missing_docs)]
 	#[structopt(flatten)]
-	pub dynamic_fee_parameters: DynamicFeeParameters,
+	pub dvm_parameters: DvmParameters,
 }
 
 #[allow(missing_docs)]
@@ -120,7 +120,7 @@ pub enum Subcommand {
 }
 
 #[derive(Debug, StructOpt)]
-pub struct DynamicFeeParameters {
+pub struct DvmParameters {
 	/// Maximum number of logs in a query.
 	#[structopt(long, default_value = "10000")]
 	pub max_past_logs: u32,
