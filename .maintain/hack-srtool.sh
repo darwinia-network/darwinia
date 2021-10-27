7b93ee34259b3ac6275378edfc58650977d72159
fewensa <fewensa@protonmail.com>
2021-10-27 18:09:07 +0800

    Test

--- .maintain/hack-srtool.sh
+++ .maintain/hack-srtool.sh
@@ -4,3 +4,6 @@
 rustup default $1
 rustup target add wasm32-unknown-unknown
 
+mkdir -p /build/target/srtool
+
+build --json --save
--- .maintain/setup-wasm.sh
+++ .maintain/setup-wasm.sh
@@ -11,15 +11,38 @@ WORK_PATH=${BIN_PATH}/../
 
 BRANCH_NAME=$(echo $GITHUB_REF | cut -d'/' -f 3)
 
+mkdir -p ${WORK_PATH}/deploy/bin
+
+PATH_REPORT=${WORK_PATH}/target/srtool/srtool-wasm-report*.txt
+
+# darwinia
+
 docker run --rm -i \
   -e PACKAGE=darwinia-runtime \
   -e VERBOSE=1 \
   -e CARGO_TERM_COLOR=always \
   -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:1.53.0 build --app \
-  | tee ${WORK_PATH}/build-darwinia-wasm.log
+  --entrypoint bash \
+  chevdor/srtool:1.53.0 /build/.maintain/hack-srtool.sh
 
+_PROPOSAL_DARWINIA_COMPACT=$(cat ${PATH_REPORT} | jq .runtimes.compact.prop)
+_PROPOSAL_DARWINIA_COMPRESSED=$(cat ${PATH_REPORT} | jq .runtimes.compressed.prop)
+_WASM_DARWINIA_COMPACT=$(cat ${PATH_REPORT} | jq .runtimes.compact.wasm)
+_WASM_DARWINIA_COMPRESSED=$(cat ${PATH_REPORT} | jq .runtimes.compressed.wasm)
+
+PATH_PROPOSAL_DARWINIA=${WORK_PATH}/deploy/bin/proposal.darwinia.txt
+echo "compact: ${_PROPOSAL_DARWINIA_COMPACT}" >> ${PATH_PROPOSAL_DARWINIA}
+echo "compressed: ${_PROPOSAL_DARWINIA_COMPRESSED}" >> ${PATH_PROPOSAL_DARWINIA}
+echo '' >> ${PATH_PROPOSAL_DARWINIA}
+
+cp ${WORK_PATH}/${_WASM_DARWINIA_COMPACT} ${WORK_PATH}/deploy/bin/
+cp ${WORK_PATH}/${_WASM_DARWINIA_COMPRESSED} ${WORK_PATH}/deploy/bin/
+
+rm -rf ${PATH_REPORT}
+
+
+# crab
 
 docker run --rm -i \
   -e PACKAGE=crab-runtime \
@@ -27,44 +50,24 @@ docker run --rm -i \
   -e CARGO_TERM_COLOR=always \
   -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:1.53.0 build --app \
-  | tee ${WORK_PATH}/build-crab-wasm.log
+  --entrypoint bash \
+  chevdor/srtool:1.53.0 /build/.maintain/hack-srtool.sh
 
+_PROPOSAL_CRAB_COMPACT=$(cat ${PATH_REPORT} | jq .runtimes.compact.prop)
+_PROPOSAL_CRAB_COMPRESSED=$(cat ${PATH_REPORT} | jq .runtimes.compressed.prop)
+_WASM_CRAB_COMPACT=$(cat ${PATH_REPORT} | jq .runtimes.compact.wasm)
+_WASM_CRAB_COMPRESSED=$(cat ${PATH_REPORT} | jq .runtimes.compressed.wasm)
 
-mkdir -p ${WORK_PATH}/deploy/bin
+PATH_PROPOSAL_CRAB=${WORK_PATH}/deploy/bin/proposal.crab.txt
+echo "compact: ${_PROPOSAL_CRAB_COMPACT}" >> ${PATH_PROPOSAL_CRAB}
+echo "compressed: ${_PROPOSAL_CRAB_COMPRESSED}" >> ${PATH_PROPOSAL_CRAB}
+echo '' >> ${PATH_PROPOSAL_CRAB}
 
-_PROPOSAL_DARWINIA=$(cat ${WORK_PATH}/build-darwinia-wasm.log | grep 'prop\s\+:')
-_PROPOSAL_CRAB=$(cat ${WORK_PATH}/build-crab-wasm.log | grep 'prop\s\+:')
+cp ${WORK_PATH}/${_WASM_CRAB_COMPACT} ${WORK_PATH}/deploy/bin/
+cp ${WORK_PATH}/${_WASM_CRAB_COMPRESSED} ${WORK_PATH}/deploy/bin/
 
-PROPOSAL_DARWINIA=0x${_PROPOSAL_DARWINIA#*0x}
-PROPOSAL_CRAB=0x${_PROPOSAL_CRAB#*0x}
+rm -rf ${PATH_REPORT}
 
-PROPOSAL_DARWINIA=$(echo ${PROPOSAL_DARWINIA} | sed 's/[^[:print:]]\[0m//g')
-PROPOSAL_CRAB=$(echo ${PROPOSAL_CRAB} | sed 's/[^[:print:]]\[0m//g')
-
-
-echo ${PROPOSAL_DARWINIA} > ${WORK_PATH}/deploy/bin/${PROPOSAL_DARWINIA}.proposal.darwinia.txt
-echo ${PROPOSAL_CRAB} > ${WORK_PATH}/deploy/bin/${PROPOSAL_CRAB}.proposal.crab.txt
-
-tree ${WORK_PATH}/target/srtool/release/
-echo  ----
-tree ${WORK_PATH}/runtime/
-
-### before srtool 2021-03-15 use this
-cp ${WORK_PATH}/target/srtool/release/wbuild/darwinia-runtime/darwinia_runtime.compact.wasm \
-  ${WORK_PATH}/deploy/bin/
-cp ${WORK_PATH}/target/srtool/release/wbuild/crab-runtime/crab_runtime.compact.wasm \
-  ${WORK_PATH}/deploy/bin/
-
-cp ${WORK_PATH}/target/srtool/release/wbuild/darwinia-runtime/target/wasm32-unknown-unknown/release/darwinia_runtime.wasm \
-  ${WORK_PATH}/deploy/bin/
-cp ${WORK_PATH}/target/srtool/release/wbuild/crab-runtime/target/wasm32-unknown-unknown/release/crab_runtime.wasm \
-  ${WORK_PATH}/deploy/bin/
-
-### after srtool 2021-03-15 use this
-#cp ${WORK_PATH}/runtime/darwinia/target/srtool/release/wbuild/darwinia-runtime/darwinia_runtime.compact.wasm \
-#  ${WORK_PATH}/deploy/bin/
-#cp ${WORK_PATH}/runtime/crab/target/srtool/release/wbuild/crab-runtime/crab_runtime.compact.wasm \
-#  ${WORK_PATH}/deploy/bin/
+# view
 
 ls ${WORK_PATH}/deploy/bin/
