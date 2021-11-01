a3a3f8342425a75b66d814d7f092c8b3f49dd7cc
fewensa <37804932+fewensa@users.noreply.github.com>
2021-11-01 10:37:39 +0800

    Update Toolchain Use `nightly-2021-04-22` (#764)
    * Try ci
    
    * Add srtool hack
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Remove build wasm in CI workflow
    
    * Add docs reference link
    
    * test without jq actions
    
    * Remove jq actions

--- .maintain/setup-wasm.sh
+++ .maintain/setup-wasm.sh
@@ -11,57 +11,62 @@ WORK_PATH=${BIN_PATH}/../
 
 BRANCH_NAME=$(echo $GITHUB_REF | cut -d'/' -f 3)
 
-## 1. chevdor/srtool:nightly-2021-02-25 have a bug, --json and --app upside down, use other version need remove it.
-EXTRA_ARGS='--json'
+mkdir -p ${WORK_PATH}/deploy/bin
+
+PATH_REPORT=${WORK_PATH}/target/srtool/srtool-wasm-report*.txt
+
+# doc: https://github.com/darwinia-network/darwinia/pull/764#discussion_r737966278
+
+# darwinia
 
 docker run --rm -i \
   -e PACKAGE=darwinia-runtime \
   -e VERBOSE=1 \
   -e CARGO_TERM_COLOR=always \
+  -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:${RUST_TOOLCHAIN} build ${EXTRA_ARGS} \
-  | tee ${WORK_PATH}/build-darwinia-wasm.log
+  --entrypoint bash \
+  chevdor/srtool:1.53.0 /build/.maintain/hack-srtool.sh
 
+_PROPOSAL_DARWINIA_COMPACT=$(cat ${PATH_REPORT} | jq -r .runtimes.compact.prop)
+_PROPOSAL_DARWINIA_COMPRESSED=$(cat ${PATH_REPORT} | jq -r .runtimes.compressed.prop)
+_WASM_DARWINIA_COMPACT=$(cat ${PATH_REPORT} | jq -r .runtimes.compact.wasm)
+_WASM_DARWINIA_COMPRESSED=$(cat ${PATH_REPORT} | jq -r .runtimes.compressed.wasm)
+
+PATH_PROPOSAL_DARWINIA=${WORK_PATH}/deploy/bin/proposal.darwinia.txt
+echo "compact: ${_PROPOSAL_DARWINIA_COMPACT}" >> ${PATH_PROPOSAL_DARWINIA}
+echo "compressed: ${_PROPOSAL_DARWINIA_COMPRESSED}" >> ${PATH_PROPOSAL_DARWINIA}
+echo '' >> ${PATH_PROPOSAL_DARWINIA}
+
+cp ${WORK_PATH}/${_WASM_DARWINIA_COMPACT} ${WORK_PATH}/deploy/bin/
+cp ${WORK_PATH}/${_WASM_DARWINIA_COMPRESSED} ${WORK_PATH}/deploy/bin/
+
+
+# crab
 
 docker run --rm -i \
   -e PACKAGE=crab-runtime \
   -e VERBOSE=1 \
   -e CARGO_TERM_COLOR=always \
+  -e RUSTC_VERSION=${RUST_TOOLCHAIN} \
   -v ${WORK_PATH}:/build \
-  chevdor/srtool:${RUST_TOOLCHAIN} build ${EXTRA_ARGS} \
-  | tee ${WORK_PATH}/build-crab-wasm.log
+  --entrypoint bash \
+  chevdor/srtool:1.53.0 /build/.maintain/hack-srtool.sh
+
+_PROPOSAL_CRAB_COMPACT=$(cat ${PATH_REPORT} | jq -r .runtimes.compact.prop)
+_PROPOSAL_CRAB_COMPRESSED=$(cat ${PATH_REPORT} | jq -r .runtimes.compressed.prop)
+_WASM_CRAB_COMPACT=$(cat ${PATH_REPORT} | jq -r .runtimes.compact.wasm)
+_WASM_CRAB_COMPRESSED=$(cat ${PATH_REPORT} | jq -r .runtimes.compressed.wasm)
+
+PATH_PROPOSAL_CRAB=${WORK_PATH}/deploy/bin/proposal.crab.txt
+echo "compact: ${_PROPOSAL_CRAB_COMPACT}" >> ${PATH_PROPOSAL_CRAB}
+echo "compressed: ${_PROPOSAL_CRAB_COMPRESSED}" >> ${PATH_PROPOSAL_CRAB}
+echo '' >> ${PATH_PROPOSAL_CRAB}
+
+cp ${WORK_PATH}/${_WASM_CRAB_COMPACT} ${WORK_PATH}/deploy/bin/
+cp ${WORK_PATH}/${_WASM_CRAB_COMPRESSED} ${WORK_PATH}/deploy/bin/
 
 
-mkdir -p ${WORK_PATH}/deploy/bin
-
-_PROPOSAL_DARWINIA=$(cat ${WORK_PATH}/build-darwinia-wasm.log | grep 'Proposal\s\+:')
-_PROPOSAL_CRAB=$(cat ${WORK_PATH}/build-crab-wasm.log | grep 'Proposal\s\+:')
-
-PROPOSAL_DARWINIA=0x${_PROPOSAL_DARWINIA#*0x}
-PROPOSAL_CRAB=0x${_PROPOSAL_CRAB#*0x}
-
-PROPOSAL_DARWINIA=$(echo ${PROPOSAL_DARWINIA} | sed 's/[^[:print:]]\[0m//g')
-PROPOSAL_CRAB=$(echo ${PROPOSAL_CRAB} | sed 's/[^[:print:]]\[0m//g')
-
-
-echo ${PROPOSAL_DARWINIA} > ${WORK_PATH}/deploy/bin/${PROPOSAL_DARWINIA}.proposal.darwinia.txt
-echo ${PROPOSAL_CRAB} > ${WORK_PATH}/deploy/bin/${PROPOSAL_CRAB}.proposal.crab.txt
-
-### before srtool 2021-03-15 use this
-cp ${WORK_PATH}/target/srtool/release/wbuild/darwinia-runtime/darwinia_runtime.compact.wasm \
-  ${WORK_PATH}/deploy/bin/
-cp ${WORK_PATH}/target/srtool/release/wbuild/crab-runtime/crab_runtime.compact.wasm \
-  ${WORK_PATH}/deploy/bin/
-
-cp ${WORK_PATH}/target/srtool/release/wbuild/darwinia-runtime/target/wasm32-unknown-unknown/release/darwinia_runtime.wasm \
-  ${WORK_PATH}/deploy/bin/
-cp ${WORK_PATH}/target/srtool/release/wbuild/crab-runtime/target/wasm32-unknown-unknown/release/crab_runtime.wasm \
-  ${WORK_PATH}/deploy/bin/
-
-### after srtool 2021-03-15 use this
-#cp ${WORK_PATH}/runtime/darwinia/target/srtool/release/wbuild/darwinia-runtime/darwinia_runtime.compact.wasm \
-#  ${WORK_PATH}/deploy/bin/
-#cp ${WORK_PATH}/runtime/crab/target/srtool/release/wbuild/crab-runtime/crab_runtime.compact.wasm \
-#  ${WORK_PATH}/deploy/bin/
+# view
 
 ls ${WORK_PATH}/deploy/bin/
