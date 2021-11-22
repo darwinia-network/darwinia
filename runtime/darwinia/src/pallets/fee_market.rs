3e7fb0942ef0e39065119c28b2f24450fe2a9d70
Xavier Lau <xavier@inv.cafe>
2021-11-22 15:15:46 +0800

    Remove `SlashPerBlock`

--- runtime/darwinia/src/pallets/fee_market.rs
+++ runtime/darwinia/src/pallets/fee_market.rs
@@ -15,7 +15,6 @@ frame_support::parameter_types! {
 	pub const AssignedRelayersNumber: u64 = 3;
 	pub const MinimumRelayFee: Balance = 15 * COIN;
 	pub const CollateralPerOrder: Balance = 100 * COIN;
-	pub const SlashPerBlock: Balance = 2 * COIN;
 	pub const Slot: BlockNumber = 50;
 
 	pub const AssignedRelayersRewardRatio: Permill = Permill::from_percent(60);
