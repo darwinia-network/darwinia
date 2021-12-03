580099839daa8db8f9cd21e50768510c60c045b8
Xavier Lau <xavier@inv.cafe>
2021-12-03 17:56:59 +0800

    Update

--- .maintain/purge-large-cache.sh
+++ .maintain/purge-large-cache.sh
@@ -4,6 +4,4 @@ cargo clean -p darwinia 2> /dev/null || true
 cargo clean -p darwinia-service 2> /dev/null || true
 cargo clean -p darwinia-runtime 2> /dev/null || true
 cargo clean -p crab-runtime 2> /dev/null || true
-cargo clean -p darwinia-cli 2> /dev/null || true
-cargo clean -p librocksdb-sys 2> /dev/null || true
 rm -rf target/debug/wbuild 2> /dev/null || true
