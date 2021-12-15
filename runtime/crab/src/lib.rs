92a47ec2c8324533ae51fa5ddf70249819214ff3
Xavier Lau <xavier@inv.cafe>
2021-12-15 23:11:58 +0800

    Bump Runtime Version

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Crab"),
 	impl_name: sp_runtime::create_runtime_str!("Darwinia Crab"),
 	authoring_version: 0,
-	spec_version: 11_7_0,
+	spec_version: 11_7_1,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
