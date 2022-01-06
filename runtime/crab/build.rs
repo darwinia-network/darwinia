4a4b18ceced3b98f0aacbafeec8678309ba21a66
bear <wpf15701504487@gmail.com>
2022-01-06 17:09:10 +0800

    Happy New Year (#825)

--- runtime/crab/build.rs
+++ runtime/crab/build.rs
@@ -1,6 +1,6 @@
 // This file is part of Darwinia.
 //
-// Copyright (C) 2018-2021 Darwinia Network
+// Copyright (C) 2018-2022 Darwinia Network
 // SPDX-License-Identifier: GPL-3.0
 //
 // Darwinia is free software: you can redistribute it and/or modify
