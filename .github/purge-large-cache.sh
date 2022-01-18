70554a66c8abe5d934f47436602b7d7f50508031
Xavier Lau <xavier@inv.cafe>
2022-01-18 10:39:41 +0800

    Fix CI

--- .github/purge-large-cache.sh
+++ .github/purge-large-cache.sh
@@ -5,3 +5,9 @@ cargo clean --release -p darwinia-service 2> /dev/null || true
 cargo clean --release -p darwinia-runtime 2> /dev/null || true
 cargo clean --release -p crab-runtime 2> /dev/null || true
 rm -rf target/release/wbuild 2> /dev/null || true
+
+cargo clean -p darwinia 2> /dev/null || true
+cargo clean -p darwinia-service 2> /dev/null || true
+cargo clean -p darwinia-runtime 2> /dev/null || true
+cargo clean -p crab-runtime 2> /dev/null || true
+rm -rf target/debug/wbuild 2> /dev/null || true
