928fdfd49c081df43a3afa8c0bfdaa8521cfd6b7
Xavier Lau <xavier@inv.cafe>
2022-03-09 16:48:17 +0800

    Update `pallet-authority-discovery`

--- runtime/darwinia/src/pallets/authority_discovery.rs
+++ runtime/darwinia/src/pallets/authority_discovery.rs
@@ -3,4 +3,10 @@ use pallet_authority_discovery::Config;
 // --- darwinia-network ---
 use crate::*;
 
-impl Config for Runtime {}
+frame_support::parameter_types! {
+	pub const MaxAuthorities: u32 = 100_000;
+}
+
+impl Config for Runtime {
+	type MaxAuthorities = MaxAuthorities;
+}
