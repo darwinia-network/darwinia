5ea35d4777af0aa5e91cfdd738a73c63c69601b7
Xavier Lau <xavier@inv.cafe>
2022-05-06 14:35:42 +0800

    Migrate Unclaims to Treasury Account

--- runtime/crab/src/migrations.rs
+++ runtime/crab/src/migrations.rs
@@ -1,24 +1,45 @@
 // --- paritytech ---
 #[allow(unused)]
 use frame_support::{migration, traits::OnRuntimeUpgrade, weights::Weight};
+use frame_support::{traits::Currency, PalletId};
+use sp_runtime::traits::AccountIdConversion;
 // --- darwinia-network ---
 #[allow(unused)]
 use crate::*;
+use darwinia_support::traits::LockableCurrency;
 
 fn migrate() -> Weight {
-	0
-	// RuntimeBlockWeights::get().max_block
+	let claims_pallet_id = PalletId(*b"da/claim");
+	let claims_pallet_account = claims_pallet_id.into_account();
+	let treasury_account = PalletId(*b"da/trsry").into_account();
+
+	// We mint this ED before. Clean it.
+	Ring::remove_lock(claims_pallet_id.0, &claims_pallet_account);
+	let _ = Ring::slash(&claims_pallet_account, 1 * COIN);
+	// Transfer all balances to treasury account.
+	let _ = Ring::transfer_all(Origin::signed(claims_pallet_account), treasury_account, false);
+
+	// 0
+	RuntimeBlockWeights::get().max_block
 }
 
 pub struct CustomOnRuntimeUpgrade;
 impl OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<(), &'static str> {
+		let claims_pallet_account = PalletId(*b"da/claim").into_account();
+
+		assert!(Ring::free_balance(&claims_pallet_account) != 0);
+
 		Ok(())
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade() -> Result<(), &'static str> {
+		let claims_pallet_account = PalletId(*b"da/claim").into_account();
+
+		assert!(Ring::free_balance(&claims_pallet_account) == 0);
+
 		Ok(())
 	}
 
