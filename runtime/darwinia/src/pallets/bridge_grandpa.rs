9c1ceb39d4714acdfb769302549757cf8a69a7bc
Xavier Lau <xavier@inv.cafe>
2022-05-27 13:45:35 +0800

    Close #887

--- runtime/darwinia/src/pallets/bridge_grandpa.rs
+++ runtime/darwinia/src/pallets/bridge_grandpa.rs
@@ -10,16 +10,12 @@ frame_support::parameter_types! {
 	// Note that once this is hit the pallet will essentially throttle incoming requests down to one
 	// call per block.
 	pub const MaxRequests: u32 = 50;
-	// Number of headers to keep.
-	//
-	// Assuming the worst case of every header being finalized, we will keep headers for at least a
-	// week.
-	pub const HeadersToKeep: u32 = 7 * bp_crab::DAYS as u32;
+	pub const CrabHeadersToKeep: u32 = 1_000;
 }
 
 impl Config<WithCrabGrandpa> for Runtime {
 	type BridgedChain = bp_crab::Crab;
-	type HeadersToKeep = HeadersToKeep;
+	type HeadersToKeep = CrabHeadersToKeep;
 	type MaxRequests = MaxRequests;
 	type WeightInfo = ();
 }
