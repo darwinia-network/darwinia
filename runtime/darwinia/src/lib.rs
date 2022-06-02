e25a19a18d7e1e0e5aa56a4f8c94dfdf5cf42dda
Xavier Lau <xavier@inv.cafe>
2022-06-02 11:24:14 +0800

    Bump Versions

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -173,7 +173,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Darwinia"),
 	impl_name: sp_runtime::create_runtime_str!("Darwinia"),
 	authoring_version: 0,
-	spec_version: 12_2_1,
+	spec_version: 12_2_2,
 	impl_version: 0,
 	#[cfg(not(feature = "disable-runtime-api"))]
 	apis: RUNTIME_API_VERSIONS,
