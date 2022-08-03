66a44c3cbd22cef1094c3e77c812dfb24492560b
Xavier Lau <xavier@inv.cafe>
2022-08-03 16:04:06 +0800

    Remove Useless Inherent Data Provider (#932)
    * Companion of paritytech/polkadot#5750
    
    * Remove Unused Params

--- node/service/src/service/mod.rs
+++ node/service/src/service/mod.rs
@@ -317,7 +317,6 @@ where
 			telemetry.as_ref().map(|x| x.handle()),
 		);
 		let slot_duration = babe_link.config().slot_duration();
-		let client_clone = client.clone();
 		let babe_config = BabeParams {
 			keystore: keystore_container.sync_keystore(),
 			client: client.clone(),
@@ -326,23 +325,17 @@ where
 			env: proposer,
 			sync_oracle: network.clone(),
 			justification_sync_link: network.clone(),
-			create_inherent_data_providers: move |parent, ()| {
-				let client_clone = client_clone.clone();
-
+			create_inherent_data_providers: move |_, ()| {
 				async move {
-					// TODO: https://github.com/paritytech/polkadot/pull/5750
-					let uncles = sc_consensus_uncles::create_uncles_inherent_data_provider(
-						&*client_clone,
-						parent,
-					)?;
 					let timestamp = sp_timestamp::InherentDataProvider::from_system_time();
 					let slot =
+
 					sp_consensus_babe::inherents::InherentDataProvider::from_timestamp_and_duration(
 						*timestamp,
 						slot_duration,
 					);
 
-					Ok((timestamp, slot, uncles))
+					Ok((timestamp, slot))
 				}
 			},
 			force_authoring,
