d571857569fa9e5cfe68be7da5d9029eb93bccdc
Xavier Lau <xavier@inv.cafe>
2022-09-30 14:13:10 +0800

    Remove storages

--- runtime/darwinia/src/migrations.rs
+++ runtime/darwinia/src/migrations.rs
@@ -23,23 +23,62 @@ impl OnRuntimeUpgrade for CustomOnRuntimeUpgrade {
 }
 
 fn migrate() -> Weight {
-	let authorities = [
-		"0x953d65e6054b7eb1629f996238c0aa9b4e2dbfe9",
-		"0x7c9b3d4cfc78c681b7460acde2801452aef073a9",
-		"0x717c38fd5fdecb1b105a470f861b33a6b0f9f7b8",
-		"0x3e25247CfF03F99a7D83b28F207112234feE73a6",
-		"0x2EaBE5C6818731E282B80De1a03f8190426e0Dd9",
-	]
-	.iter()
-	.filter_map(|s| array_bytes::hex_into(s).ok())
-	.collect::<Vec<_>>();
-
-	if !authorities.is_empty() {
-		if let Ok(authorities) = frame_support::BoundedVec::try_from(authorities) {
-			<darwinia_ecdsa_authority::Authorities<Runtime>>::put(authorities.clone());
-			<darwinia_ecdsa_authority::NextAuthorities<Runtime>>::put(authorities);
-		}
-	}
+	[
+		// TODO: check if we have left some balances locks
+		(
+			b"EcdsaRelayAuthority",
+			[
+				b"Candidates",
+				b"Authorities",
+				b"NextAuthorities",
+				b"NextTerm",
+				b"AuthoritiesToSign",
+				b"MmrRootsToSignKeys",
+				b"MmrRootsToSign",
+				b"SubmitDuration",
+			],
+		),
+		(
+			// FrameV1 name.
+			b"DarwiniaEthereumRelay",
+			[
+				b"ConfirmedHeaderParcels",
+				b"ConfirmedBlockNumbers",
+				b"BestConfirmedBlockNumber",
+				b"ConfirmedDepth",
+				b"DagsMerkleRoots",
+				b"ReceiptVerifyFee",
+				b"PendingRelayHeaderParcels",
+			],
+		),
+		// TODO: check if we have left some balances locks
+		(
+			// FrameV1 name.
+			b"DarwiniaRelayerGameInstance1",
+			[
+				b"RelayHeaderParcelToResolve",
+				b"Affirmations",
+				b"BestConfirmedHeaderId",
+				b"RoundCounts",
+				b"AffirmTime",
+				b"GamesToUpdate",
+				b"Stakes",
+				b"GameSamplePoints",
+			],
+		),
+		(
+			b"EthereumBacking",
+			[
+				b"TokenRedeemAddress",
+				b"DepositRedeemAddress",
+				b"SetAuthoritiesAddress",
+				b"RingTokenAddress",
+				b"KtonTokenAddress",
+				b"RedeemStatus",
+				b"LockAssetEvents",
+			],
+		),
+	];
 
 	// 0
 	RuntimeBlockWeights::get().max_block
