ee2063331916b996cdc544cd33b2bb1264425cf6
Xavier Lau <xavier@inv.cafe>
2022-10-26 10:31:15 +0800

    Bump `array-bytes`

--- runtime/darwinia/src/migrations.rs
+++ runtime/darwinia/src/migrations.rs
@@ -87,14 +87,14 @@ fn migrate() -> Weight {
 	// - 0xBd1a110ec476b4775c43905000288881367B1a88
 	// - 2qSbd2umtD4KmV2X89YfqmCQgDraEabAaLNFiR96xUJ1m31G
 	// - 0x64766d3a00000000000000bd1a110ec476b4775c43905000288881367b1a88ad
-	let dest = array_bytes::hex_into_unchecked::<AccountId, 32>(
+	let dest = array_bytes::hex_n_into_unchecked::<AccountId, 32>(
 		"0x64766d3a00000000000000bd1a110ec476b4775c43905000288881367b1a88ad",
 	);
 	// Ethereum backing account:
 	// - modlda/ethbk
 	// - 2qeMxq616BhqvTW8a1bp2g7VKPAmpda1vXuAAz5TxV5ehivG
 	// - 0x6d6f646c64612f657468626b0000000000000000000000000000000000000000
-	let origin = array_bytes::hex_into_unchecked::<AccountId, 32>(
+	let origin = array_bytes::hex_n_into_unchecked::<AccountId, 32>(
 		"0x6d6f646c64612f657468626b0000000000000000000000000000000000000000",
 	);
 	<darwinia_balances::Pallet<Runtime, RingInstance>>::transfer_all(
