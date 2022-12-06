dc9c57f1c9f7b048daed88b0ecf5a8d9e88756f4
Xavier Lau <xavier@inv.cafe>
2022-12-06 10:39:26 +0800

    Correct block time (#93)

--- core/primitives/src/lib.rs
+++ core/primitives/src/lib.rs
@@ -67,7 +67,7 @@ pub type Block = sp_runtime::generic::Block<Header, sp_runtime::OpaqueExtrinsic>
 /// up by `pallet_aura` to implement `fn slot_duration()`.
 ///
 /// Change this to adjust the block time.
-pub const MILLISECS_PER_BLOCK: u64 = 6_000;
+pub const MILLISECS_PER_BLOCK: u64 = 12_000;
 
 // NOTE: Currently it is not possible to change the slot duration after the chain has started.
 //       Attempting to do so will brick block production.
