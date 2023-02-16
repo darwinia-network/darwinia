62c77fd9f10e3efe0d528fb3ad37098b07e6a721
bear <boundless.forest@outlook.com>
2023-02-16 11:42:48 +0800

    Add evm estimate gas tests (#282)
    * Add estimate gas tests
    
    * Fix CI

--- tests/ethereum/test-gas.ts
+++ tests/ethereum/test-gas.ts
@@ -2,7 +2,14 @@ import Web3 from "web3";
 import { describe } from "mocha";
 import { step } from "mocha-steps";
 import { expect } from "chai";
-import { HOST_HTTP_URL, FAITH, FAITH_P, EXTRINSIC_GAS_LIMIT, customRequest } from "../config";
+import {
+	HOST_HTTP_URL,
+	FAITH,
+	FAITH_P,
+	EXTRINSIC_GAS_LIMIT,
+	customRequest,
+	DEFAULT_GAS,
+} from "../config";
 import { incrementerInfo } from "./contracts/contracts_info";
 import { AbiItem } from "web3-utils";
 
@@ -11,6 +18,30 @@ describe("Test transaction gas limit", () => {
 	web3.eth.accounts.wallet.add(FAITH_P);
 	const inc = new web3.eth.Contract(incrementerInfo.abi as AbiItem[]);
 	const data = inc.deploy({ data: incrementerInfo.bytecode, arguments: [5] });
+
+	it("Test contract create estimate gas", async () => {
+		expect(
+			await web3.eth.estimateGas({
+				from: FAITH,
+				data: data.encodeABI(),
+			})
+		).to.equal(144926);
+	}).timeout(60000);
+
+	it("Test contract call estimate gas", async () => {
+		let tx = await web3.eth.accounts.signTransaction(
+			{
+				from: FAITH,
+				data: data.encodeABI(),
+				gas: DEFAULT_GAS,
+			},
+			FAITH_P
+		);
+		let receipt = await web3.eth.sendSignedTransaction(tx.rawTransaction);
+		inc.options.address = receipt.contractAddress;
+
+		expect(await inc.methods.increment(3).estimateGas()).to.equal(28340);
+	}).timeout(60000);
 
 	it("Test transaction gas limit < `EXTRINSIC_GAS_LIMIT`", async () => {
 		let tx = await web3.eth.accounts.signTransaction(
