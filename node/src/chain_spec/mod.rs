5861bbd44c9b902b396d6a363a76320090cbcdc0
Xavier Lau <xavier@inv.cafe>
2023-03-09 04:10:26 +0800

    Format

--- node/src/chain_spec/mod.rs
+++ node/src/chain_spec/mod.rs
@@ -74,7 +74,7 @@ use sp_core::{Pair, Public};
 #[allow(unused)]
 pub type DummyChainSpec = sc_chain_spec::GenericChainSpec<(), Extensions>;
 
-const TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+const TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit";
 
 const ALITH: &str = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac";
 const BALTATHAR: &str = "0x3Cd0A705a2DC65e5b1E1205896BaA2be8A07c6e0";
