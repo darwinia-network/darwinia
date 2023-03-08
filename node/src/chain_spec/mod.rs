13969e184197ec5bce24c5566faed6eb9fb09a57
Xavier Lau <xavier@inv.cafe>
2023-03-09 04:07:31 +0800

    Update bootnodes and telemetry

--- node/src/chain_spec/mod.rs
+++ node/src/chain_spec/mod.rs
@@ -74,6 +74,8 @@ use sp_core::{Pair, Public};
 #[allow(unused)]
 pub type DummyChainSpec = sc_chain_spec::GenericChainSpec<(), Extensions>;
 
+const TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
+
 const ALITH: &str = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac";
 const BALTATHAR: &str = "0x3Cd0A705a2DC65e5b1E1205896BaA2be8A07c6e0";
 const CHARLETH: &str = "0x798d4Ba9baf0064Ec19eB4F0a1a45785ae9D6DFc";
