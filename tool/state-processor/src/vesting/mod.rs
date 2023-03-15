a4e77bf705cc61ba28b02c946d83f5cd6ace05a3
Xavier Lau <xavier@inv.cafe>
2023-03-15 13:50:43 +0800

    Fix storage type

--- tool/state-processor/src/vesting/mod.rs
+++ tool/state-processor/src/vesting/mod.rs
@@ -5,13 +5,13 @@ impl<S> Processor<S> {
 	pub fn process_vesting(&mut self) -> &mut Self {
 		// Storage items.
 		// https://github.dev/darwinia-network/substrate/blob/darwinia-v0.12.5/frame/vesting/src/lib.rs#L188
-		let mut vestings = <Map<VestingInfo>>::default();
+		let mut vestings = <Map<Vec<VestingInfo>>>::default();
 
 		log::info!("take solo `Vesting::Vesting`");
 		self.solo_state.take_map(b"Vesting", b"Vesting", &mut vestings, get_hashed_key);
 
 		log::info!("adjust solo `VestingInfo`s");
-		vestings.iter_mut().for_each(|(_, v)| v.adjust());
+		vestings.iter_mut().for_each(|(_, v)| v.iter_mut().for_each(|v| v.adjust()));
 
 		log::info!("set `AccountMigration::Vestings`");
 		{
