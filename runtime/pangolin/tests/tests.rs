1bb6d0f090c488eb60f03d3a79d5bfbfd8afe6de
bear <boundless.forest@outlook.com>
2023-03-22 13:35:35 +0800

    Add `multiplier_tests`

--- runtime/pangolin/tests/tests.rs
+++ runtime/pangolin/tests/tests.rs
@@ -3,3 +3,38 @@ pub mod mock;
 darwinia_common_runtime::impl_account_migration_tests! {}
 
 darwinia_common_runtime::impl_evm_tests! {}
+
+#[cfg(test)]
+mod multiplier_tests {
+	// darwinia
+	use crate::mock::*;
+	// substrate
+	use frame_support::{dispatch::DispatchClass, pallet_prelude::Weight};
+	use polkadot_runtime_common::{MinimumMultiplier, SlowAdjustingFeeUpdate, TargetBlockFullness};
+	use sp_runtime::traits::Convert;
+
+	fn run_with_system_weight<F>(w: Weight, mut assertions: F)
+	where
+		F: FnMut() -> (),
+	{
+		let mut t: sp_io::TestExternalities =
+			frame_system::GenesisConfig::default().build_storage::<Runtime>().unwrap().into();
+		t.execute_with(|| {
+			System::set_block_consumed_resources(w, 0);
+			assertions()
+		});
+	}
+
+	#[test]
+	fn multiplier_can_grow_from_zero() {
+		let minimum_multiplier = MinimumMultiplier::get();
+		let target = TargetBlockFullness::get()
+			* RuntimeBlockWeights::get().get(DispatchClass::Normal).max_total.unwrap();
+		// if the min is too small, then this will not change, and we are doomed forever.
+		// the weight is 1/100th bigger than target.
+		run_with_system_weight(target.saturating_mul(101) / 100, || {
+			let next = SlowAdjustingFeeUpdate::<Runtime>::convert(minimum_multiplier);
+			assert!(next > minimum_multiplier, "{:?} !>= {:?}", next, minimum_multiplier);
+		})
+	}
+}
