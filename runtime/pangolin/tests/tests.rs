e5bea732414837b1eafe415fcc81931401a46bde
Xavier Lau <xavier@inv.cafe>
2023-03-27 17:13:30 +0800

    Remove cfg

--- runtime/pangolin/tests/tests.rs
+++ runtime/pangolin/tests/tests.rs
@@ -1,10 +1,7 @@
 pub mod mock;
 
-#[cfg(test)]
 darwinia_common_runtime::impl_account_migration_tests! {}
 
-#[cfg(test)]
 darwinia_common_runtime::impl_evm_tests! {}
 
-#[cfg(test)]
 darwinia_common_runtime::impl_fee_tests! {}
