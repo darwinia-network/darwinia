2eb5e4ca5cb3dd9261dc9a10f096955ec946fcaf
Xavier Lau <xavier@inv.cafe>
2023-04-20 16:42:23 +0800

    Update Darwinia genesis (#1110)
    * Update Darwinia genesis
    
    * Remove

--- tool/state-processor/src/main.rs
+++ tool/state-processor/src/main.rs
@@ -41,7 +41,7 @@ fn main() -> Result<()> {
 	// <Processor<Crab>>::new()?.test().process().save()?;
 	// <Processor<Crab>>::new()?.process().save()?;
 	<Processor<Darwinia>>::new()?.test().process().save()?;
-	// <Processor<Darwinia>>::new()?.process().save()?;
+	<Processor<Darwinia>>::new()?.process().save()?;
 
 	Ok(())
 }
