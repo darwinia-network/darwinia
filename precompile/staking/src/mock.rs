682fa093400888b3e0893aa938a803870f5a7434
Xavier Lau <xavier@inv.cafe>
2023-04-20 13:39:28 +0800

    Fix

--- precompile/staking/src/mock.rs
+++ precompile/staking/src/mock.rs
@@ -223,7 +223,7 @@ impl darwinia_staking::Stake for KtonStaking {
 impl darwinia_staking::Config for TestRuntime {
 	type Deposit = Deposit;
 	type Kton = KtonStaking;
-	type MaxDeposits = <Deposit as darwinia_deposit::Config>::MaxDeposits;
+	type MaxDeposits = <Self as darwinia_deposit::Config>::MaxDeposits;
 	type MaxUnstakings = frame_support::traits::ConstU32<16>;
 	type MinStakingDuration = frame_support::traits::ConstU64<3>;
 	type PayoutFraction = PayoutFraction;
