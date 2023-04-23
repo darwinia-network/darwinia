35b270f40ca69f8df70bc7580aa424ed5ede6493
bear <boundless.forest@outlook.com>
2023-04-23 15:06:46 +0800

    Add runtime api implementation for all runtimes

--- runtime/crab/src/lib.rs
+++ runtime/crab/src/lib.rs
@@ -36,7 +36,11 @@ pub mod weights;
 pub use darwinia_common_runtime::*;
 pub use dc_primitives::*;
 
+// frontier
+use pallet_ethereum::{Call::transact, Transaction as EthereumTransaction};
 // substrate
+use sp_core::H256;
+use sp_runtime::traits::Block as BlockT;
 use sp_std::prelude::*;
 
 /// Block type as expected by this runtime.
@@ -522,6 +526,109 @@ sp_api::impl_runtime_apis! {
 			)
 		}
 	}
+
+	impl moonbeam_rpc_primitives_debug::DebugRuntimeApi<Block> for Runtime {
+		fn trace_transaction(
+			extrinsics: Vec<<Block as BlockT>::Extrinsic>,
+			traced_transaction: &EthereumTransaction,
+		) -> Result<
+			(),
+			sp_runtime::DispatchError,
+		> {
+			#[cfg(feature = "evm-tracing")]
+			{
+				use moonbeam_evm_tracer::tracer::EvmTracer;
+				// use xcm_primitives::{
+				// 	ETHEREUM_XCM_TRACING_STORAGE_KEY,
+				// 	EthereumXcmTracingStatus
+				// };
+				use frame_support::storage::unhashed;
+
+				// Tell the CallDispatcher we are tracing a specific Transaction.
+				// unhashed::put::<EthereumXcmTracingStatus>(
+				// 	ETHEREUM_XCM_TRACING_STORAGE_KEY,
+				// 	&EthereumXcmTracingStatus::Transaction(traced_transaction.hash()),
+				// );
+
+				// Apply the a subset of extrinsics: all the substrate-specific or ethereum
+				// transactions that preceded the requested transaction.
+				for ext in extrinsics.into_iter() {
+					let _ = match &ext.0.function {
+						RuntimeCall::Ethereum(transact { transaction }) => {
+							if transaction == traced_transaction {
+								EvmTracer::new().trace(|| Executive::apply_extrinsic(ext));
+								return Ok(());
+							} else {
+								Executive::apply_extrinsic(ext)
+							}
+						}
+						_ => Executive::apply_extrinsic(ext),
+					};
+					// if let Some(EthereumXcmTracingStatus::TransactionExited) = unhashed::get(
+					// 	ETHEREUM_XCM_TRACING_STORAGE_KEY
+					// ) {
+					// 	return Ok(());
+					// }
+				}
+				Err(sp_runtime::DispatchError::Other(
+					"Failed to find Ethereum transaction among the extrinsics.",
+				))
+			}
+			#[cfg(not(feature = "evm-tracing"))]
+			Err(sp_runtime::DispatchError::Other(
+				"Missing `evm-tracing` compile time feature flag.",
+			))
+		}
+
+		fn trace_block(
+			extrinsics: Vec<<Block as BlockT>::Extrinsic>,
+			known_transactions: Vec<H256>,
+		) -> Result<
+			(),
+			sp_runtime::DispatchError,
+		> {
+			#[cfg(feature = "evm-tracing")]
+			{
+				use moonbeam_evm_tracer::tracer::EvmTracer;
+				// use xcm_primitives::EthereumXcmTracingStatus;
+
+				// Tell the CallDispatcher we are tracing a full Block.
+				// frame_support::storage::unhashed::put::<EthereumXcmTracingStatus>(
+				// 	xcm_primitives::ETHEREUM_XCM_TRACING_STORAGE_KEY,
+				// 	&EthereumXcmTracingStatus::Block,
+				// );
+
+				let mut config = <Runtime as pallet_evm::Config>::config().clone();
+				config.estimate = true;
+
+				// Apply all extrinsics. Ethereum extrinsics are traced.
+				for ext in extrinsics.into_iter() {
+					match &ext.0.function {
+						RuntimeCall::Ethereum(transact { transaction }) => {
+							if known_transactions.contains(&transaction.hash()) {
+								// Each known extrinsic is a new call stack.
+								EvmTracer::emit_new();
+								EvmTracer::new().trace(|| Executive::apply_extrinsic(ext));
+							} else {
+								let _ = Executive::apply_extrinsic(ext);
+							}
+						}
+						_ => {
+							let _ = Executive::apply_extrinsic(ext);
+						}
+					};
+				}
+
+				Ok(())
+			}
+			#[cfg(not(feature = "evm-tracing"))]
+			Err(sp_runtime::DispatchError::Other(
+				"Missing `evm-tracing` compile time feature flag.",
+			))
+		}
+	}
+
+
 
 	#[cfg(feature = "runtime-benchmarks")]
 	impl frame_benchmarking::Benchmark<Block> for Runtime {
