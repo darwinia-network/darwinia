646ebca9e99c727febbf410007f0967610e5d39c
Xavier Lau <xavier@inv.cafe>
2023-04-28 15:02:35 +0800

    Disable burn (#1135)

--- runtime/pangoro/src/pallets/treasury.rs
+++ runtime/pangoro/src/pallets/treasury.rs
@@ -22,14 +22,13 @@ use crate::*;
 frame_support::parameter_types! {
 	pub const TreasuryPalletId: frame_support::PalletId = frame_support::PalletId(*b"da/trsry");
 	pub const ProposalBond: sp_runtime::Permill = sp_runtime::Permill::from_percent(5);
-	pub const Burn: sp_runtime::Permill = sp_runtime::Permill::from_percent(1);
 }
 
 // In order to use `Tips`, which bounded by `pallet_treasury::Config` rather
 // `pallet_treasury::Config<I>` Still use `DefaultInstance` here instead `Instance1`
 impl pallet_treasury::Config for Runtime {
 	type ApproveOrigin = RootOrAtLeastThreeFifth<CouncilCollective>;
-	type Burn = Burn;
+	type Burn = ();
 	type BurnDestination = ();
 	type Currency = Balances;
 	type MaxApprovals = ConstU32<100>;
