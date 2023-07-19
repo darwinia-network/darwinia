11fa7dc33f44ef4bc4855351e867baa434623661
Xavier Lau <xavier@inv.cafe>
2023-07-19 12:25:31 +0800

    Optimize

--- .github/shrink-cache.sh
+++ .github/shrink-cache.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-for p in darwinia polkadot-cli librocksdb-sys wasm-opt-sys
+for p in darwinia
+# for p in darwinia polkadot-cli librocksdb-sys wasm-opt-sys
 do
-  cargo clean -p ${p} 2> /dev/null || true
-  cargo clean -p ${p} --release 2> /dev/null || true
-done
-for r in darwinia crab pangoro pangolin
-do
-  cargo clean -p ${r}-runtime 2> /dev/null || true
-  cargo clean -p ${r}-runtime --release 2> /dev/null || true
+  cargo clean -p ${p} --profile ci-dev 2> /dev/null || true
 done
 
 # ---
 
 for r in darwinia crab pangoro pangolin
 do
-  rm -rf target/debug/wbuild/${r}-runtime 2> /dev/null || true
-  rm -rf target/release/wbuild/${r}-runtime 2> /dev/null || true
+  rm -rf target/ci-dev/wbuild/${r}-runtime 2> /dev/null || true
+done
+
+for r in polkadot kusama westend rococo
+do
+  rm -rf target/ci-dev/wbuild/${r}-runtime/target/release 2> /dev/null || true
 done
