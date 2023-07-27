d598398cbcf45f9565107405f77ab32ce84e2e89
Xavier Lau <xavier@inv.cafe>
2023-07-27 13:09:24 +0800

    Revamp all workflows

--- .github/shrink-cache.sh
+++ .github/shrink-cache.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 for p in darwinia
-# for p in darwinia polkadot-cli librocksdb-sys wasm-opt-sys
 do
   cargo clean -p ${p} --profile ci-dev 2> /dev/null || true
 done
@@ -13,7 +12,7 @@ do
   rm -rf target/ci-dev/wbuild/${r}-runtime 2> /dev/null || true
 done
 
-for r in polkadot kusama westend rococo
-do
-  rm -rf target/ci-dev/wbuild/${r}-runtime/target/release 2> /dev/null || true
-done
+# for r in polkadot kusama westend rococo
+# do
+#   rm -rf target/ci-dev/wbuild/${r}-runtime/target/release 2> /dev/null || true
+# done
