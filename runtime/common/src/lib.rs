12420fadae6c8e6ba9b46d6b1d1aad798eaadadf
Xavier Lau <xavier@inv.cafe>
2023-07-28 21:08:42 +0800

    Fmt

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -58,7 +58,6 @@ pub const EXISTENTIAL_DEPOSIT: Balance = 0;
 #[cfg(feature = "runtime-benchmarks")]
 pub const EXISTENTIAL_DEPOSIT: Balance = 1;
 
-
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
 ///
