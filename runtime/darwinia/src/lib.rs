8205c47608b4bdedcf1f2c0b1a316c15696eb795
Xavier Lau <xavier@inv.cafe>
2023-11-17 15:49:22 +0800

    Add migrations

--- runtime/darwinia/src/lib.rs
+++ runtime/darwinia/src/lib.rs
@@ -69,7 +69,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	migration::CustomOnRuntimeUpgrade,
+	(migration::CustomOnRuntimeUpgrade, darwinia_staking::migration::v1::MigrateToV1<Runtime>),
 >;
 
 /// Darwinia proposal base fee.
