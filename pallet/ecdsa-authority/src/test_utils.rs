3a55abc417f096c7d43a39c0229b5373ad3ea78c
Xavier Lau <xavier@inv.cafe>
2023-11-20 10:19:42 +0800

    Fix compile

--- pallet/ecdsa-authority/src/test_utils.rs
+++ pallet/ecdsa-authority/src/test_utils.rs
@@ -23,7 +23,7 @@ pub(crate) fn gen_pair(byte: u8) -> (SecretKey, AccountId) {
 	let seed = iter::repeat(byte).take(32).collect::<Vec<_>>();
 	let secret_key = SecretKey::parse_slice(&seed).unwrap();
 	let public_key = PublicKey::from_secret_key(&secret_key).serialize();
-	let address = array_bytes::slice_n_into_unchecked::<20, _, _>(
+	let address = array_bytes::slice_n_into_unchecked::<_, _, 20>(
 		&hashing::keccak_256(&public_key[1..65])[12..],
 	);
 
