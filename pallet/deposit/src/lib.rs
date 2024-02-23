05a6555b1fd9b33719d60b32a5b993fd8bb50564
Xavier Lau <xavier@inv.cafe>
2024-02-23 10:39:49 +0800

    Fix `claim_with_penalty`
    Signed-off-by: Xavier Lau <xavier@inv.cafe>

--- pallet/deposit/src/lib.rs
+++ pallet/deposit/src/lib.rs
@@ -282,6 +282,7 @@ pub mod pallet {
 				.max(1) * 3;
 
 			T::Kton::burn(&who, kton_penalty)?;
+			T::Ring::transfer(&account_id(), &who, d.value, AllowDeath)?;
 			Self::deposit_event(Event::DepositClaimedWithPenalty {
 				owner: who,
 				deposit_id: id,
