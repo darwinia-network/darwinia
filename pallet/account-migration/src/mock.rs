f597effd0726ee13631b6390197cb5f2cabb4e47
Xavier Lau <xavier@inv.cafe>
2024-04-17 19:28:11 +0800

    Release `v6.6.2`

--- pallet/account-migration/src/mock.rs
+++ pallet/account-migration/src/mock.rs
@@ -53,7 +53,7 @@ pub const VERSION: sp_version::RuntimeVersion = sp_version::RuntimeVersion {
 	spec_name: sp_runtime::create_runtime_str!("Darwinia2"),
 	impl_name: sp_runtime::create_runtime_str!("DarwiniaOfficialRust"),
 	authoring_version: 0,
-	spec_version: 6_6_1_0,
+	spec_version: 6_6_2_0,
 	impl_version: 0,
 	apis: sp_version::create_apis_vec!([]),
 	transaction_version: 0,
