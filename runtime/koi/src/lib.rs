720fd47b2118a5752553fadb6ea2b432e91e3826
Xavier Lau <xavier@inv.cafe>
2024-06-20 18:18:10 +0800

    paritytech/polkadot-sdk#1492

--- runtime/koi/src/lib.rs
+++ runtime/koi/src/lib.rs
@@ -310,6 +310,16 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 
+	impl sp_genesis_builder::GenesisBuilder<Block> for Runtime {
+		fn create_default_config() -> Vec<u8> {
+			sp_genesis_builder::create_default_config::<RuntimeGenesisConfig>()
+		}
+
+		fn build_config(config: Vec<u8>) -> sp_genesis_builder::Result {
+			sp_genesis_builder::build_config::<RuntimeGenesisConfig>(config)
+		}
+	}
+
 	impl cumulus_primitives_core::CollectCollationInfo<Block> for Runtime {
 		fn collect_collation_info(header: &<Block as sp_runtime::traits::Block>::Header) -> cumulus_primitives_core::CollationInfo {
 			ParachainSystem::collect_collation_info(header)
