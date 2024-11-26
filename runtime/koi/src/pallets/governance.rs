f8a1b8b7965e2105e7fa61d18579648ffd167274
Xavier Lau <x@acg.box>
2024-11-27 01:29:35 +0800

    Update whitelist origin

--- runtime/koi/src/pallets/governance.rs
+++ runtime/koi/src/pallets/governance.rs
@@ -82,7 +82,9 @@ impl custom_origins::Config for Runtime {}
 
 frame_support::ord_parameter_types! {
 	// 0x005493b5658e6201F06FE2adF492610635505F4C.
-	pub const RingDaoAccount: AccountId = AccountId::from([0, 84, 147, 181, 101, 142, 98, 1, 240, 111, 226, 173, 244, 146, 97, 6, 53, 80, 95, 76]);
+	pub const DispatchWhitelistedDao: AccountId = AccountId::from([
+		0, 84, 147, 181, 101, 142, 98, 1, 240, 111, 226, 173, 244, 146, 97, 6, 53, 80, 95, 76
+	]);
 }
 
 // The purpose of this pallet is to queue calls to be dispatched as by root later => the Dispatch
@@ -94,7 +96,7 @@ impl pallet_whitelist::Config for Runtime {
 	type DispatchWhitelistedOrigin = RootOrDiverse<
 		frame_support::traits::EitherOfDiverse<
 			WhitelistedCaller,
-			frame_system::EnsureSignedBy<RingDaoAccount, Self::AccountId>,
+			frame_system::EnsureSignedBy<DispatchWhitelistedDao, Self::AccountId>,
 		>,
 	>;
 	type Preimages = Preimage;
