df28cf19aef582958f819f9275c8565c72fb3ec4
Xavier Lau <x@acg.box>
2024-12-17 17:33:57 +0800

    Companion of paritytech/polkadot-sdk#3844

--- runtime/crab/src/pallets/message_queue.rs
+++ runtime/crab/src/pallets/message_queue.rs
@@ -21,6 +21,7 @@ use crate::*;
 
 impl pallet_message_queue::Config for Runtime {
 	type HeapSize = sp_core::ConstU32<{ 64 * 1024 }>;
+	type IdleMaxServiceWeight = pallet_config::MessageQueueServiceWeight;
 	type MaxStale = sp_core::ConstU32<8>;
 	#[cfg(feature = "runtime-benchmarks")]
 	type MessageProcessor = pallet_message_queue::mock_helpers::NoopMessageProcessor<
